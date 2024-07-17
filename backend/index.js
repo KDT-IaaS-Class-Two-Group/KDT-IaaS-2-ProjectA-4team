3aefcee197507df858f23021f22fb10756e5e3a4
yuxincxoi <yjyj0774@gmail.com>
2024-07-17 16:41:03 +0900

    :sparkles: 최유진 : 데이터베이스 연결

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,15 @@ let data = [
   { id: 2, name: "커피먹어" },
 ];
 
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/rockcodersERP').then(() => console.log('MongoDB is connected !')).catch(() => console.log(error));
+
 const server = http.createServer((req, res) => {
   // CORS 헤더 추가
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  
+
   if (req.url === "/endpoint" && req.method === "GET") {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify(data));
