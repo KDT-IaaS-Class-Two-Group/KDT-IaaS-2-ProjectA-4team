db918e935ff2995cdc397f4c808eac79fafaeb93
yuxincxoi <yjyj0774@gmail.com>
2024-07-17 15:02:41 +0900

    :package: 최유진 : front server 수정

--- front/index.js
+++ front/index.js
@@ -9,32 +9,39 @@ const path = require("path");
  * * 서버생성
  */
 const server = http.createServer((req, res) => {
-  const parsedUrl = url.parse(req.url, true);
+  const parsedUrl = url.parse(req.url);
+  let pathname = `./public${parsedUrl.pathname}`;
+  if (pathname === './public/') {
+    pathname = './public/index.html';
+  }
 
-  if (parsedUrl.pathname === "/") {
-    const indexPath = path.join(__dirname, "public", "index.html");
-    fs.readFile(indexPath, (err, data) => {
+  const ext = path.parse(pathname).ext;
+  const map = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'application/javascript',
+  };
+
+  fs.exists(pathname, function (exist) {
+    if (!exist) {
+      res.statusCode = 404;
+      res.end(`File ${pathname} not found!`);
+      return;
+    }
+
+    fs.readFile(pathname, function (err, data) {
       if (err) {
-        res.writeHead(500);
-        res.end("Internal Server Error");
+        res.statusCode = 500;
+        res.end(`Error getting the file: ${err}.`);
       } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
+        res.setHeader('Content-type', map[ext] || 'text/plain');
         res.end(data);
       }
     });
-  } else if (parsedUrl.pathname === "/api/data") {
-    // API 엔드포인트 처리
-    const responseData = { message: "데이터를 가져왔습니다." };
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(responseData));
-  } else {
-    // 에러처리
-    res.writeHead(404);
-    res.end("Not Found");
-  }
+  });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 server.listen(PORT, () => {
-  console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
+  console.log(`프론트엔드 서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
 });
