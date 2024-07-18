5d9c26e7a3c3a12f21f7e1a82763d75fc931cbc2
최유진 <yjyj0774@gmail.com>
2024-07-18 16:24:39 +0900

    :card_file_box: 최유진 : Product 컬렉션에 basic bun 데이터 추가

--- backend/addExampleData.js
+++ backend/addExampleData.js
@@ -1,9 +1,11 @@
 const Member = require("../shared/Member");
+const Product = require("../shared/Product");
 const Role = require("../shared/Role");
 
 const addExampleData = () => {
   try {
     addMemberData();
+    addRoleData();
   } catch (err) {
     console.log(err);
   }
@@ -42,6 +44,18 @@ const addRoleData = async () => {
 
   await admin.save();
   await user.save();
+};
+const addProductData = async () => {
+  const basicBun = new Product({
+    productID: 1,
+    productName: "기본 빵",
+    unitPrice: 1000,
+    quantity: 200,
+    restockDate: new Date("2024-07-15"),
+    expirationDate: new Date("2024-08-13"),
+  });
+
+  await basicBun.save();
 };
 
 module.exports = addExampleData;
