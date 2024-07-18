bfa03663672ba45b13898bf42089159bd941634a
최유진 <yjyj0774@gmail.com>
2024-07-18 15:54:35 +0900

    :card_file_box: 최유진 : Member 컬렉션에 member1 데이터 추가

--- backend/addExampleData.js
+++ backend/addExampleData.js
@@ -1,3 +1,19 @@
-const addExampleData = async () => {};
+const Member = require("../shared/Member");
+
+const addExampleData = async () => {
+  try {
+    const member1 = new Member({
+      memberId: 1,
+      name: "유호영",
+      email: "hy@merge.com",
+      password: "hy123",
+      roleID: 1,
+    });
+
+    await member1.save();
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = addExampleData;
