6ce0d83225c240460b0d5c10b264aa5c9ea6a087
최유진 <yjyj0774@gmail.com>
2024-07-18 16:53:58 +0900

    :white_check_mark: 최유진 : 컬렉션 생성 확인을 위한 함수 제거

--- backend/index.js
+++ backend/index.js
@@ -17,27 +17,10 @@ mongoose
   .connect("mongodb://localhost:27017/rockcodersERP")
   .then(() => {
     console.log("MongoDB is connected !");
-    checkCollection();
   })
   .catch(() => console.log(error));
 
 addExampleData();
-
-// * 컬렉션 확인 함수
-async function checkCollection() {
-  try {
-    const members = await Member.find();
-    console.log("Existing members:", members);
-
-    const products = await Product.find();
-    console.log("Existing products:", products);
-
-    const roles = await Role.find();
-    console.log("Existing roles:", roles);
-  } catch (error) {
-    console.error("Error checking Member collection:", error);
-  }
-}
 
 const server = http.createServer((req, res) => {
   // CORS 헤더 추가
