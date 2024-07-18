6cd9f51415d76ff9654369a83596908dadb6a447
이종수 <whdtnxd@gmail.com>
2024-07-18 12:45:52 +0900

    :sparkles: 이종수: 유저의 입력값과 DB에서의 유저 ID를 비교하는 모듈 생성

--- front/src/client/modules/checkUserFromDB.js
+++ front/src/client/modules/checkUserFromDB.js
@@ -1,1 +1,7 @@
-export default (value) => {};
+/**
+ * @eonduck2 24.07.18
+ * * 입력된 값과 DB에서의 유저 아이디의 일치를 확인
+ * @param { string } value 유저의 입력값
+ * @param { string } userIdFromDB DB에서 전달받은 유저의 ID
+ */
+export default (value, userIdFromDB) => value === userIdFromDB;
