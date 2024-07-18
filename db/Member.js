f68e0d6ff9124427cb84a8e39803fa4d395e1577
최유진 <yjyj0774@gmail.com>
2024-07-18 12:04:16 +0900

    :package: 최유진 : member 컬렉션 스키마 정의

--- db/Member.js
+++ db/Member.js
@@ -1,1 +1,9 @@
 const mongoose = require("mongoose");
+
+const memberSchema = new mongoose.Schema({
+  memberId: { type: Number, required: true, unique: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  roleID: { type: Number, required: true },
+});
