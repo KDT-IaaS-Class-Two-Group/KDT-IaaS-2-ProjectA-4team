0b07c71ca199cd4f0e0c8784cce35cc78380a195
최유진 <yjyj0774@gmail.com>
2024-07-18 16:11:31 +0900

    :package: 최유진 : 회원정보 데이터 추가하는 함수 생성

--- backend/addExampleData.js
+++ backend/addExampleData.js
@@ -1,27 +1,31 @@
 const Member = require("../shared/Member");
 
-const addExampleData = async () => {
+const addExampleData = () => {
   try {
-    const member1 = new Member({
-      memberId: 1,
-      name: "유호영",
-      email: "hy@merge.com",
-      password: "hy123",
-      roleID: 1,
-    });
-    const member2 = new Member({
-      memberId: 2,
-      name: "이종수",
-      email: "js@kirby.com",
-      password: "js123",
-      roleID: 2,
-    });
-
-    await member1.save();
-    await member2.save();
+    addMemberData();
   } catch (err) {
     console.log(err);
   }
+};
+
+const addMemberData = async () => {
+  const member1 = new Member({
+    memberId: 1,
+    name: "유호영",
+    email: "hy@merge.com",
+    password: "hy123",
+    roleID: 1,
+  });
+  const member2 = new Member({
+    memberId: 2,
+    name: "이종수",
+    email: "js@kirby.com",
+    password: "js123",
+    roleID: 2,
+  });
+
+  await member1.save();
+  await member2.save();
 };
 
 module.exports = addExampleData;
