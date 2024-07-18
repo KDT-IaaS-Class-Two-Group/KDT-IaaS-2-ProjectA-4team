9175576ab84657be958792bd829bb68b65b5a5b3
최유진 <yjyj0774@gmail.com>
2024-07-18 12:41:40 +0900

    :package: 최유진 : product 컬렉션 스키마 정의

--- shared/Product.js
+++ shared/Product.js
@@ -1,1 +1,10 @@
 const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  productID: { type: Number, required: true, unique: true },
+  productName: { type: String, required: true, unique: true },
+  unitPrice: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+  restockDate: { type: Date },
+  expirationDate: { type: Date, required: true },
+});
