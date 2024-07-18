2b0ce051246aa935d010b11f27d91a8a56b91467
최유진 <yjyj0774@gmail.com>
2024-07-18 15:55:05 +0900

    :card_file_box: 최유진 : Member 컬렉션에 member2 데이터 추가

--- backend/addExampleData.js
+++ backend/addExampleData.js
@@ -9,8 +9,16 @@ const addExampleData = async () => {
       password: "hy123",
       roleID: 1,
     });
+    const member2 = new Member({
+      memberId: 2,
+      name: "이종수",
+      email: "js@kirby.com",
+      password: "js123",
+      roleID: 2,
+    });
 
     await member1.save();
+    await member2.save();
   } catch (err) {
     console.log(err);
   }
