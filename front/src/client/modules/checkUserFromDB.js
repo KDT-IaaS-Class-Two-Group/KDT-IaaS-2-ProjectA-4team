38bebf1cf6d5221fb697865248bbe26822b3407a
이종수 <whdtnxd@gmail.com>
2024-07-18 12:51:51 +0900

    :bulb: 이종수: 정규식 평가 모듈 주석 추가

--- front/src/client/modules/checkUserFromDB.js
+++ front/src/client/modules/checkUserFromDB.js
@@ -3,5 +3,6 @@
  * * 입력된 값과 DB에서의 유저 아이디의 일치를 확인
  * @param { string } value 유저의 입력값
  * @param { string } userIdFromDB DB에서 전달받은 유저의 ID
+ * @returns { boolean } 비교값에 관한 불리언 값
  */
 export default (value, userIdFromDB) => value === userIdFromDB;
