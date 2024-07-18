a19f9bfdcf1c8c2ed0d8ff1b5eef2004e5e3fdd4
최유진 <yjyj0774@gmail.com>
2024-07-18 14:21:16 +0900

    :package: 최유진 : Role 컬렉션 스키마 정의

--- shared/Role.js
+++ shared/Role.js
@@ -1,1 +1,7 @@
 const mongoose = require("mongoose");
+
+const roleSchema = new mongoose.Schema({
+  roleID: { type: Number, require: true, unique: true },
+  roleName: { type: String, require: true },
+  permission: { type: String, require: true },
+});
