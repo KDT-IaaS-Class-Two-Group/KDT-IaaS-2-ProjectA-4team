482c4c50b88341829be16b1570d51c526c77a674
moonhr <moonhr153@gmail.com>
2024-07-19 17:05:02 +0900

    :package:문혜림: 클라이언트에서 백서버로 보내는 패치

--- front/public/join.script.js
+++ front/public/join.script.js
@@ -1,4 +1,40 @@
-import { ValidateName } from "../module/Vaildate/ValidateName";
-import { ValidateEmail } from "../module/Vaildate/ValidateEmail";
-import { ValidatePassword } from "../module/Vaildate/ValidatePassword";
-import { validatePasswordCheck } from "../module/Vaildate/ValidatePasswordCheck";
+// import { ValidateName } from "../module/Vaildate/ValidateName";
+// import { ValidateEmail } from "../module/Vaildate/ValidateEmail";
+// import { ValidatePassword } from "../module/Vaildate/ValidatePassword";
+// import { validatePasswordCheck } from "../module/Vaildate/ValidatePasswordCheck";
+// import { getEffectiveTypeParameterDeclarations } from "typescript";
+
+document
+  .getElementById("join-button")
+  .addEventListener("click", async function () {
+    const form = document.getElementById("join-container");
+    const formData = new FormData(form);
+    const userData = {
+      name: formData.get("name"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+      testPw: formData.get("test-pw"),
+    };
+    console.log("userData:", userData);
+    try {
+      const response = await fetch("http://localhost:4000/join", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+      console.log("res:", response);
+
+      const result = await response.json();
+
+      console.log("result:", result);
+      alert(result.message);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("An error occurred while sending the data.");
+    }
+  });
