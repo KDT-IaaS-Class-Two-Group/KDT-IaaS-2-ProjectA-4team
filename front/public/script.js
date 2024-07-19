3539c4309fca1a3b3050762da70500bca1bee517
jojayeon <jojayeon6152@gmail.com>
2024-07-19 19:11:39 +0900

    :sparkles: 조자연 : test - js 이벤트 작성

--- front/public/script.js
+++ front/public/script.js
@@ -8,3 +8,18 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     .catch(error => console.error('Error fetching data:', error));
 });
+
+const root = document.getElementById('root');
+const submit1 = document.getElementById('submit1');
+
+submit1.addEventListener('click', () => {
+  // localStorage.setItem// 로컬스토리지
+  console.log("A")
+  fetch('http://localhost:3000/end')
+  .then(response => response.json())
+    .then(data => {
+      console.log("data")
+      console.log(data)
+    })
+    .catch(error => console.error('Error fetching data:', error));
+});
