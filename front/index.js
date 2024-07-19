24378914ccc7eec962fe2d20a91dbc43679a2fe8
moonhr <moonhr153@gmail.com>
2024-07-19 23:53:06 +0900

    :package:문헤림: 프론트서버에서의 cors제거

--- front/index.js
+++ front/index.js
@@ -1,11 +1,11 @@
 //프론트 서버 진입점
 const path = require("path");
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/join", (req, res) => {
