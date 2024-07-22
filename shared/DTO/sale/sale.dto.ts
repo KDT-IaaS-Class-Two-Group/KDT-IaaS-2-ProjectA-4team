367b61f46a76626a8b96bff8041cf6f08f8e8e73
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-22 18:17:04 +0900

    :heavy_plus_sign:유호영:saleDTO

---
+++ shared/DTO/sale/sale.dto.ts
@@ -0,0 +1,29 @@
+import ISale from "../../../db/sale/sale.interface";
+import { SaleProductDTO } from "./saleProduct.dto";
+
+
+export class SaleDTO implements ISale {
+  public saleID: number;
+  public memberID: string;
+  public products: SaleProductDTO[];
+  public saleDate: Date;
+  public totalPrice: number;
+
+  constructor(sale: ISale) {
+    this.saleID = sale.saleID;
+    this.memberID = sale.memberID;
+    this.products = sale.products.map(product => new SaleProductDTO(product));
+    this.saleDate = sale.saleDate;
+    this.totalPrice = sale.totalPrice;
+  }
+
+  public toJSON(): object {
+    return {
+      saleID: this.saleID,
+      memberID: this.memberID,
+      products: this.products.map(product => product.toJSON()),
+      saleDate: this.saleDate,
+      totalPrice: this.totalPrice
+    };
+  }
+}
