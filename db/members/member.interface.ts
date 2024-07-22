69f23ef0e94966b5507d4b7fde42edaa794741ca
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-22 17:06:49 +0900

    :heavy_plus_sign:유호영:product schema정의

--- db/members/member.interface.ts
+++ db/members/member.interface.ts
@@ -1,7 +1,7 @@
-import IEmail from "../../shared/DTO/interfaces/Email.interface";
-import IName from "../../shared/DTO/interfaces/Name.interface";
-import IPassword from "../../shared/DTO/interfaces/Password.interface";
-import IRoleID from "../../shared/DTO/interfaces/RoleID.interface";
+import IEmail from "../../shared/DTO/members/interface/Email.interface";
+import IName from "../../shared/DTO/members/interface/Name.interface";
+import IPassword from "../../shared/DTO/members/interface/Password.interface";
+import IRoleID from "../../shared/DTO/members/interface/RoleID.interface";
 
 /**
  * @crystal23733 24.07.22
