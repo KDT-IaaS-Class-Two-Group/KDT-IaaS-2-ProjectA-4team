025c3d53443588619904a06e018e343a2dbcb715
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-22 18:24:18 +0900

    :heavy_plus_sign:유호영:sale model 정의

--- db/role/model/role.model.ts
+++ db/role/model/role.model.ts
@@ -1,12 +1,12 @@
-/**
- * @crystal23733 24.07.22
- * * role model 정의
- */
 
 import mongoose from "mongoose";
 import IRole from "../Role.interface";
 import roleSchema from "../schema/role.schema";
 
+/**
+ * @crystal23733 24.07.22
+ * * role model 정의
+ */
 const Role = mongoose.model<IRole>("Role", roleSchema);
 
 export default Role;
