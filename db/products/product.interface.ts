2acd9b9866a0273e5f28e7c05f79574475684435
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-22 17:02:52 +0900

    :heavy_plus_sign:유호영:product interface정의

---
+++ db/products/product.interface.ts
@@ -0,0 +1,15 @@
+import IExpirationDate from "../../shared/DTO/products/interface/ExpirationDate.interface";
+import IProductID from "../../shared/DTO/products/interface/ProductID.interface";
+import IProductName from "../../shared/DTO/products/interface/ProductName.interface";
+import IQuantity from "../../shared/DTO/products/interface/Quantity.interface";
+import IRestockData from "../../shared/DTO/products/interface/RestockData.interface";
+import IUnitPrice from "../../shared/DTO/products/interface/UnitPrice.interface";
+
+/**
+ * @crystal23733 24.07.22
+ * * product interface
+ */
+
+export default interface IProduct extends IProductID, IProductName, IUnitPrice, IQuantity, IRestockData, IExpirationDate{
+
+}
