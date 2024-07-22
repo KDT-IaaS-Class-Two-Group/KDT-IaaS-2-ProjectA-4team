dad8b9b01c51e8800d1ea581e154e496104924c2
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-22 12:20:11 +0900

    :heavy_plus_sign:유호영:Next, Nest 세팅

---
+++ app/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    // 다른 규칙들을 여기에 추가할 수 있습니다.
+  },
+};
