895b064c941614e2ef5b30e4e252e35fc47d802f
jojayeon <jojayeon6152@gmail.com>
2024-07-23 15:52:50 +0900

    :package: 조자연 :cjs방식으로 변경

--- db/read/Allread.js
+++ db/read/Allread.js
@@ -1,16 +1,20 @@
 //읽기 하는 부분 스키마불러와서 작업
 //전부찾기
-import memberSchema from "./Schema"
+
+/**
+ * 
+ */
+const member = require("./Schema");
 
 const allread = async (name) => {
   try {
     const query = name ? { name } : {};
-    const user = await memberSchema.find({query});
-    console.log("성공")
-    return user
+    const user = await member.find({query});
+    console.log("성공");
+    return user;
   } catch (err) {
     console.log(err);
   }
 };
 
-export default allread
+module.exports = allread;
