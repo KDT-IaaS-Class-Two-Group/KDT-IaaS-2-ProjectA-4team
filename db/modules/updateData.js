10e21b1c0a1d82c9749f723326f461d2fc0d8dc2
최유진 <yjyj0774@gmail.com>
2024-07-23 11:58:31 +0900

    :bulb: 최유진 : 주석 추가

--- db/modules/updateData.js
+++ db/modules/updateData.js
@@ -1,5 +1,13 @@
 import mongoose from "mongoose";
 
+/**
+ * @yuxincxoi 24.07.23
+ * * 특정 데이터 업데이트
+ * @param {string} collection 업데이트 할 컬렉션 이름
+ * @param {string} field 업데이트 할 필드 이름
+ * @param {string|number|object|array} value 필드에서 찾을 값
+ * @param {object} data 변경할 데이터
+ */
 const updateOneDocument = async (collection, field, value, data) => {
   try {
     // * 업데이트할 문서를 찾기 위한 조건을 정의
