d10b558740814910d97899d6bd76ae51a55f08f6
최유진 <yjyj0774@gmail.com>
2024-07-23 11:48:18 +0900

    :sparkles: 최유진 : 업데이트할 문서를 찾기위한 필터 생성

--- db/modules/updateData.js
+++ db/modules/updateData.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 
 const updateOneDocument = async (collection, field, value, data) => {
   try {
+    // * 업데이트할 문서를 찾기 위한 조건을 정의
+    const filter = {};
+    filter[field] = value;
   } catch {
     console.error("Error : ", err);
   } finally {
