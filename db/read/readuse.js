90268205e3c4f627740bb1bf7d71250b802270c3
jojayeon <jojayeon6152@gmail.com>
2024-07-23 15:53:05 +0900

    :package: 조자연 :cjs방식으로 변경

--- db/read/readuse.js
+++ db/read/readuse.js
@@ -2,8 +2,8 @@
 // 몽고서버랑 read부분을 가져와서 
 //서버안에 성공하면 read 불러오기 
 
-import mongoserver from "./mongoserver";
-import allread from "./Allread";
+const mongoserver = require("./mongoserver");
+const allread = require("./Allread");
 
 mongoserver()
   .then(async() => {
