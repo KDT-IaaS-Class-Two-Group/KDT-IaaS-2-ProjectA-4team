716a7c7523e7b59859c7af6b7e5b9a0f695a283d
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-23 11:51:48 +0900

    :sparkles:유호영:인덱스 조회, 삭제 기능 추가

--- db/utils/indexUtil.util.ts
+++ db/utils/indexUtil.util.ts
@@ -1,8 +1,32 @@
 import mongoose from "mongoose";
 
 export default class {
-  public createIndex(modelName:string, fieldName:string, options?:object):void{
+  public async createIndex(modelName:string, fieldName:string, options?:object):Promise<void>{
     const model = mongoose.model(modelName);
-    model.schema.index({[fieldName] : 1}, options);
-  }
+    try{
+      model.schema.index({[fieldName] : 1}, options);
+      await model.syncIndexes();
+      console.log(`${fieldName}의 인덱스를 생성하였습니다.`);
+    } catch(error) {
+      console.error(`${fieldName}의 인덱스를 삭제하였습니다.`);
+    }
+  };
+  public async dropIndex(modelName: string, indexName: string): Promise<void> {
+    const model = mongoose.model(modelName);
+    try {
+      await model.collection.dropIndex(indexName);
+      console.log(`Index ${indexName} dropped for ${modelName}`);
+    } catch (error) {
+      console.error(`Error dropping index ${indexName} for ${modelName}:`, error);
+    }
+  };
+  public async listIndexes(modelName: string): Promise<void> {
+    const model = mongoose.model(modelName);
+    try {
+      const indexes = await model.collection.indexes();
+      console.log(`Indexes for ${modelName}:`, indexes);
+    } catch (error) {
+      console.error(`Error listing indexes for ${modelName}:`, error);
+    }
+  };
 }
