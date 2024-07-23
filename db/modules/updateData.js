a729e50a5f7ade14cc26922f88701ce58e4935a2
최유진 <yjyj0774@gmail.com>
2024-07-23 12:39:50 +0900

    :package: 최유진 : 조건에 맞는 모든 문서를 업데이트하는 함수 생성

--- db/modules/updateData.js
+++ db/modules/updateData.js
@@ -25,4 +25,14 @@ const updateOneDocument = async (collectionName, field, value, data) => {
   }
 };
 
-module.exports = updateOneDocument;
+/**
+ * @yuxincxoi 24.07.23
+ * * 여러개의 특정 데이터 업데이트
+ * @param {string} collection 업데이트 할 컬렉션 이름
+ * @param {string} field 업데이트 할 필드 이름
+ * @param {string|number|object|array} value 필드에서 찾을 값
+ * @param {object} data 변경할 데이터
+ */
+const updateAllDocument = async () => {};
+
+module.exports = { updateOneDocument, updateAllDocument };
