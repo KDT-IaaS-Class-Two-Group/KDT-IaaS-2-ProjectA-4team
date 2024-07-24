c3420c4cf212453b55a6fd596c373bd76418a7f5
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-24 17:51:13 +0900

    :art:유호영:myPage password-change-box 완료

--- client/src/pages/myPage.tsx
+++ client/src/pages/myPage.tsx
@@ -1,3 +1,4 @@
+import { Input } from "components/ui/input";
 import React from "react";
 import UserHeaderComponent from "src/components/users/userHeaderComponent";
 
@@ -6,15 +7,25 @@ const MyPage:React.FC = () => {
     <div id="root" className="w-screen h-screen flex flex-col justify-center items-center">
       <UserHeaderComponent />
       <div className="h-90% w-80% flex flex-col justify-center items-center">
-        <div id="content-header" className="w-full h-15% flex">
-          <a href="/userPage" className="text-xl">&larr; 돌아가기</a>
+        <div id="content-header" className="w-full h-10% flex">
+          <a href="/userPage" className="text-xl h-2">&larr; 돌아가기</a>
         </div>
-        <div id="password-change-box" className="h-30% w-full">
-          <div></div>
-          <div></div>
-          <div></div>
+        <form action="" id="password-change-box" className="h-30% w-full">
+          <p>비밀번호 변경</p>
+          <Input type="password" placeholder="변경할 비밀번호를 입력해주세요." name="password" className="text-xl" />
+          <Input type="password" placeholder="비밀번호 확인" name="checkPassword" className="text-xl" />
+          <Input type="submit" value="변경하기" />
+        </form>
+        <div id="order-details" className="h-50% w-full">
+          <div id="order-details__header" className="">
+            <h1 className="font-bold text-lg">장바구니</h1>
+          </div>
+          <div id="order-details__content" className="flex flex-col h-full overflow-y-scroll">
+            <div className="order-details__items">
+              <h1></h1>
+            </div>
+          </div>
         </div>
-        <div id="order-details" className="h-50% w-full"></div>
       </div>
     </div>
   );
