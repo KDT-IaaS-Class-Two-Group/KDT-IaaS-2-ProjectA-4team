6e026e191c790d564b8b006b1170d75bd38e5ace
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-24 12:48:39 +0900

    :heavy_plus_sign:유호영:기본 전역컴포넌트 생성

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,17 @@
+<<<<<<< HEAD
 import { AppProps } from "next/app";
 import "../../style/globals.css";
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 export default MyApp;
+=======
+import { AppProps } from 'next/app';
+import '../../style/globals.css';
+
+function MyApp({ Component, pageProps }:AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
+>>>>>>> 43d67f4 (:heavy_plus_sign:유호영:기본 전역컴포넌트 생성)
