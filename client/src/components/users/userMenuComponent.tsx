d0b450788d87fc953a4cde6bdbaafd1896d65fd3
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-24 14:32:39 +0900

    :art:유호영:usermenubar 제작중

--- client/src/components/users/userMenuComponent.tsx
+++ client/src/components/users/userMenuComponent.tsx
@@ -1,1 +1,12 @@
+import { MenubarMenu, MenubarTrigger } from "components/ui/menubar";
 import React from "react";
+
+const UserMenuComponent:React.FC = () => {
+  return (
+    <MenubarMenu>
+      <MenubarTrigger>빵</MenubarTrigger>
+    </MenubarMenu>
+  );
+}
+
+export default UserMenuComponent;
