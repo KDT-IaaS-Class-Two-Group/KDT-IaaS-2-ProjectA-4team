d78525c4c4ffc251b5a4afa0e6260d326be06615
jojayeon <jojayeon6152@gmail.com>
2024-07-24 14:54:38 +0900

    :bulb: 조자연: 설명추가

--- client/src/components/title.tsx
+++ client/src/components/title.tsx
@@ -1,3 +1,9 @@
+/** @jojayeon 24.07.24
+ *  * h1(titleComponenent)를 div에 넣은 형태
+ *  * 박스 필요할까 생각되어서 
+ */
+
+
 import React from "react";
 import TitleComponent from "./titleComponent";
 import test from "./static/TestText";
@@ -9,3 +15,4 @@ const Title:React.FC = () => {
     </div>
   )
 }
+export default Title;
