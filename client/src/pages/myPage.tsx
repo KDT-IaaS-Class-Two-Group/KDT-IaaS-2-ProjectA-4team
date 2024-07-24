0f5d52cf31bd4afadd287338f99d4e42c1e1a733
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-24 12:49:33 +0900

    :art:유저페이지 헤더 컴포넌트 생성

---
+++ client/src/pages/myPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import UserHeaderComponent from "src/components/userHeaderComponent";
+
+const MyPage:React.FC = () => {
+  return (
+    <UserHeaderComponent />
+  );
+}
+
+export default MyPage;
