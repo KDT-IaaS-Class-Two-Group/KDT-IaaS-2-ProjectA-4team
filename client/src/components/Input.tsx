83ac03d4d07c7e317d7f6dcab924ee63ba12f299
최유진 <yjyj0774@gmail.com>
2024-07-24 12:56:07 +0900

    :sparkles: 최유진 : input 컴포넌트 생성

---
+++ client/src/components/Input.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import { Input } from "../../components/ui/input";
+
+const InputComponent: FC = () => {
+  return (
+    <div>
+      <Input />
+    </div>
+  );
+};
+
+export default InputComponent;
