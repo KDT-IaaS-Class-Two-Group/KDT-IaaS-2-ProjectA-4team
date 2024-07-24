b0ad7637705ce1a842ae1c34968ee0d34711a903
eonduck2 <whdtnxd@gmail.com>
2024-07-24 13:59:11 +0900

    :package: 이종수: 모달 컴포넌트 제작 완료

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import { AppProps } from 'next/app';
-import '../../style/globals.css';
+import { AppProps } from "next/app";
+import "../../style/global.css";
 
-function MyApp({ Component, pageProps }:AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
