434667837a499923794a837670184dbd8f5a2d9a
최유진 <yjyj0774@gmail.com>
2024-07-24 13:10:21 +0900

    :hammer: 최유진 : _app.tsx 파일 추가

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from "next/app";
-import "../../style/global.css";
+import "../../style/globals.css";
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
