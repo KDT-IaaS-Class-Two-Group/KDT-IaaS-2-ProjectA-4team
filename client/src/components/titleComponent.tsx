4a0d6a2bb4adf650bbc2ed9c90761d801a351fc8
jojayeon <jojayeon6152@gmail.com>
2024-07-24 14:55:38 +0900

    :package: 조자연: 스타일,설명추가

--- client/src/components/titleComponent.tsx
+++ client/src/components/titleComponent.tsx
@@ -1,4 +1,6 @@
-//타이틀 컨포넌트 파일
+/** @jojayeon 24.07.24
+ *  타이틀 h1태그
+ */
 import React from "react";
 
 interface h1 {
@@ -7,7 +9,7 @@ interface h1 {
 
 const TitleComponent:React.FC<h1> = ({titletext}) => {
   return (
-    <h1>{titletext}</h1>
+    <h1 className=" text-center text-xl font-bold w-50 h-10 mt-3">{titletext}</h1>
   )
 }
 
