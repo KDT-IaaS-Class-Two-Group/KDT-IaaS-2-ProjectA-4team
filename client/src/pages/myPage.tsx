aeb9c6f204787f59e3f57e86cac81f4a75f417d1
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-24 16:35:57 +0900

    :art:myPage content-box 제작중

--- client/src/pages/myPage.tsx
+++ client/src/pages/myPage.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import UserHeaderComponent from "src/components/users/userHeaderComponent";
-import UserMenuComponent from "src/components/users/userMenuComponent";
 
 const MyPage:React.FC = () => {
   return (
-    <>
+    <div id="root" className="w-screen h-screen flex flex-col justify-center items-center">
       <UserHeaderComponent />
-      <UserMenuComponent />
-    </>
+      <div className="h-90% w-80% flex flex-col justify-center items-center">
+        <div id="content-header" className="w-full h-15% flex">
+          <a href="/userPage" className="text-xl">&larr; 돌아가기</a>
+        </div>
+        <div id="password-change-box" className="h-30% w-full">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div id="order-details" className="h-50% w-full"></div>
+      </div>
+    </div>
   );
 }
 
