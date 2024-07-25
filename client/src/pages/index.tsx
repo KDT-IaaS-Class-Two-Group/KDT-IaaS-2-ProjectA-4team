ace2afea423c1cbf958b6dd08c801c33618b041b
moonhr <moonhr153@gmail.com>
2024-07-25 10:49:39 +0900

    :package:문혜림: 기존 내용 복구

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,24 +1,5 @@
-import CustomButton from "src/components/CustomButton";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Modal from "src/components/Modal";
 
-import React from "react";
-
-const MyComponent: React.FC = () => {
-  return (
-    <Router>
-      <div>
-        <CustomButton
-          to="/"
-          text="Go to Home Page"
-          variant="secondary"
-          size="sm"
-        />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-};
-
-export default MyComponent;
+export default function Home() {
+  return <div>ㅎㅇ</div>;
+}
