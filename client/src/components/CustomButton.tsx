474336b37d45747d2377c2351c3ce575a321186f
moonhr <moonhr153@gmail.com>
2024-07-25 12:55:21 +0900

    :package:문혜림 : 버튼 타입 설정 추가

--- client/src/components/CustomButton.tsx
+++ client/src/components/CustomButton.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { Button } from "components/ui/button";
-import { useNavigate } from "react-router-dom";
+import { useRouter } from "next/router";
 import { IButton } from "src/types/IButton.type";
 
 const CustomButton: React.FC<
   IButton & { type?: "button" | "submit" | "reset" }
 > = ({ text, variant, size, to, type = "button" }) => {
-  const navigate = useNavigate();
+  const router = useRouter();
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     // 회원가입, 로그인 등 submit과 페이지 이동이 둘 다 필요한 경우.
     if (type === "submit" && to) {
       event.preventDefault();
-      navigate(to);
+      setTimeout(() => {
+        router.push(to);
+      }, 0);
       // 페이지 이동만 요구할 경우.
     } else if (to) {
       event.preventDefault();
-      navigate(to);
+      router.push(to);
+    } else if (type === "reset") {
+      console.log("폼리셋");
     }
   };
 
