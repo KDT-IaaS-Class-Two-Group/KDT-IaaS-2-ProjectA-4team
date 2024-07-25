c789a90b1e930f3129bba20c18be9e8ad8a83fe9
최유진 <yjyj0774@gmail.com>
2024-07-25 13:00:14 +0900

    :sparkles: 최유진 : 장바구니 개별 메뉴 컴포넌트 생성 완료

--- client/src/components/CartItem.tsx
+++ client/src/components/CartItem.tsx
@@ -1,22 +1,35 @@
 import React, { FC } from "react";
 
+interface CartItemComponentProps {
+  menu: string;
+  price: string;
+  count: number;
+}
+
 /**
  * @yuxincxoi 24.07.25
  * * 장바구니에 들어가는 개별 메뉴 컴포넌트
+ * @param {string} menu 메뉴명
+ * @param {string} price 가격
+ * @param {number} count 구매 개수
  * @returns { JSXElement }
  */
 
-const CartItemComponent: FC = () => {
+const CartItemComponent: FC<CartItemComponentProps> = ({
+  menu,
+  price,
+  count,
+}) => {
   return (
     <div className="px-10 py-3 w-72">
       <div className="flex justify-between text-base">
-        <div id="menu">징징이다리 패티</div>
-        <div id="price">4000원</div>
+        <div id="menu">{menu}</div>
+        <div id="price">{price}</div>
       </div>
       <div className="flex justify-start">
         <div id="minusMenu" className="bg-slate-600 w-6 h-6"></div>
         <div id="count" className="mx-3 font-light text-s">
-          1
+          {count}
         </div>
         <div id="plusMenu" className="bg-slate-600 w-6 h-6"></div>
       </div>
