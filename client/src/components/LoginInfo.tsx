9ad5edb998707f1aa755231c4657b7ac7fdc9601
최유진 <yjyj0774@gmail.com>
2024-07-25 11:47:38 +0900

    :wrench: 최유진 : 로그인 정보 컴포넌트 파일 생성

---
+++ client/src/components/LoginInfo.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+
+/**
+ * @yuxincxoi 24.07.25
+ * * 사이드바의 로그인 정보를 나타내는 컴포넌트
+ * * "안녕하세요 ! [이메일]님"
+ * @returns { JSXElement }
+ */
+
+const LoginInfoComponent: FC = () => {
+  return <div></div>;
+};
+
+export default LoginInfoComponent;
