0912ba78f4fc1f9ea861a906a9f5ef60e83ca717
jojayeon <jojayeon6152@gmail.com>
2024-07-25 18:57:58 +0900

    package: 조자연 : 전체만 조회하는 걸로 변경

--- db/read/Allreaduse.js
+++ db/read/Allreaduse.js
@@ -1,6 +1,6 @@
 /** @jojayeon 20.07.23
  * * 사용 하는 부분도 만들어본 것
- * * 지금 은 비여있음 -> 전체를 가져오겠다.
+ * * 전체 조회
  */
 
 const mongoserver = require("./mongoserver");
@@ -10,7 +10,6 @@ mongoserver()
   .then(async() => {
     try {
       await allread(); 
-      //"name"하면 불러와지고 비우면 전체 불러오기
     } catch (err) {
       console.log(err + "문서 조회 할떄 오류임")
     }finally {
