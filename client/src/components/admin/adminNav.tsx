c0f4ea9b181bc773fabe6587cfc668387e28730d
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-25 17:36:40 +0900

    :truck:유호영:nav 이름 변경

---
+++ client/src/components/admin/adminNav.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginInfoComponent from "../LoginInfo";
+
+const AdminNav:React.FC = () => {
+  return(
+    <div id="aside">
+      <LoginInfoComponent email="rockCoders" />
+    </div>
+  );
+}
+
+export default AdminNav;
