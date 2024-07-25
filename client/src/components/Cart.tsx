a5c36237df6923883eb09867a3c49973376cb6d3
최유진 <yjyj0774@gmail.com>
2024-07-25 14:39:59 +0900

    :wrench: 최유진 : 장바구니 컴포넌트 파일 생성

---
+++ client/src/components/Cart.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+/**
+ * @yuxincxoi 24.07.25
+ * * 장바구니 컴포넌트
+ * @returns { JSXElement }
+ */
+
+const Cart: FC = () => {
+  return <div></div>;
+};
+
+export default Cart;
