f41cd4e356f528d84dee2f7b0b7ef3c854b00bf6
moonhr <moonhr153@gmail.com>
2024-07-25 11:58:48 +0900

    :package: 문혜림: 버튼의 타입 추가함.
    추가된 타입은 button, submit, reset
    1. submit과 페이지 이동을 요구할 경우
    <CustomButton text="login" variant="default" size="default" type="submit" to="/home" />
    2. 필드를 비우는 버튼이 필요할 경우
    <CustomButton text="Reset" variant="destructive" size="default" type="reset" />
    3. 페이지 이동만 필요할 경우
    <CustomButton text="Go to About" variant="secondary" size="default" to="/about" />
    
    타입설정은 필수가 아님.

--- client/src/components/CustomButton.tsx
+++ client/src/components/CustomButton.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 import { Button } from "components/ui/button";
 import { useNavigate } from "react-router-dom";
-import { IButton } from "src/types/Ibutton.type";
+import { IButton } from "src/types/IButton.type";
 
-const CustomButton: React.FC<IButton> = ({ to, text, variant, size }) => {
+const CustomButton: React.FC<
+  IButton & { type?: "button" | "submit" | "reset" }
+> = ({ text, variant, size, to, type = "button" }) => {
   const navigate = useNavigate();
 
-  const handleClick = () => {
-    navigate(to);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    // 회원가입, 로그인 등 submit과 페이지 이동이 둘 다 필요한 경우.
+    if (type === "submit" && to) {
+      event.preventDefault();
+      navigate(to);
+      // 페이지 이동만 요구할 경우.
+    } else if (to) {
+      event.preventDefault();
+      navigate(to);
+    }
   };
 
   return (
-    <Button onClick={handleClick} variant={variant} size={size}>
+    <Button onClick={handleClick} variant={variant} size={size} type={type}>
       {text}
     </Button>
   );
