03af701b9828fc3034957ba03f77e33468e8cf18
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-25 17:36:40 +0900

    :truck:유호영:nav 이름 변경

---
+++ client/src/pages/admin.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminNav from "src/components/admin/adminNav";
+
+const Admin:React.FC = () => {
+  return (
+    <div>
+      <AdminNav />
+    </div>
+  );
+}
+
+export default Admin;
