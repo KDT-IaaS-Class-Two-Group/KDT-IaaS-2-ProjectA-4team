251b7abed6e10f441123f1ff7e038efb0b2e9217
moonhr <moonhr153@gmail.com>
2024-07-25 09:32:59 +0900

    :memo:문혜림: 버튼컴포 확인

---
+++ client/src/pages/_app/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from "../../../components/ui/button";
+import React from "react";
+
+const MyComponent: React.FC = () => {
+  return (
+    <Button variant="deepnavy" size="lg">
+      Button
+    </Button>
+  );
+};
+export default MyComponent;
