d6b58176ff89de54779cbeff807ff8a6f067fe6d
최유진 <yjyj0774@gmail.com>
2024-07-25 12:31:44 +0900

    :wrench: 최유진 : 장바구니에 들어가는 개별 메뉴 컴포넌트 파일 생성

---
+++ client/src/components/CartItem.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+/**
+ * @yuxincxoi 24.07.25
+ * * 장바구니에 들어가는 개별 메뉴 컴포넌트
+ * @returns { JSXElement }
+ */
+
+const CartItemComponent: FC = () => {
+  return <div></div>;
+};
+
+export default CartItemComponent;
