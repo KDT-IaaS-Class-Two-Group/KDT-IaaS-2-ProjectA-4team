1a1ebac069d192acce4728318c26e9aa7c84da3a
최유진 <yjyj0774@gmail.com>
2024-07-25 10:38:45 +0900

    :bulb: 주석 추가

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,3 +1,11 @@
+/**
+ * @yuxincxoi 24.07.25
+ * * 사용자페이지 메뉴에서 사용될 카드 컴포넌트
+ * @param { string } title 메뉴명
+ * @param { string } content 가격
+ * @returns { JSXElement }
+ */
+
 import React, { FC } from "react";
 import {
   Card,
