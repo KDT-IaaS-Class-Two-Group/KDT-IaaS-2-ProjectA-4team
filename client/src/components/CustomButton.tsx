a35bc88f919c316b4c621f88e0a16d54e1c880c9
moonhr <moonhr153@gmail.com>
2024-07-25 15:00:29 +0900

    :package: 문혜림: 버튼 컴포넌트 수정

--- client/src/components/CustomButton.tsx
+++ client/src/components/CustomButton.tsx
@@ -1,34 +1,18 @@
-import React from "react";
-import { Button } from "components/ui/button";
-import { useRouter } from "next/router";
-import { IButton } from "src/types/IButton.type";
+import React, { FC } from "react";
+import { Button, ButtonProps } from "components/ui/button";
 
-const CustomButton: React.FC<
-  IButton & { type?: "button" | "submit" | "reset" }
-> = ({ text, variant, size, to, type = "button" }) => {
-  const router = useRouter();
-
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    // 회원가입, 로그인 등 submit과 페이지 이동이 둘 다 필요한 경우.
-    if (type === "submit" && to) {
-      event.preventDefault();
-      setTimeout(() => {
-        router.push(to);
-      }, 0);
-      // 페이지 이동만 요구할 경우.
-    } else if (to) {
-      event.preventDefault();
-      router.push(to);
-    } else if (type === "reset") {
-      console.log("폼리셋");
-    }
-  };
-
+/**
+ * @moonhr 24.07.25
+ * * shadcn ui로 기본적으로 제공된 button
+ * @param { string } props button 태그가 가질 수 있는 속성들
+ * * 예) type="button", className="bg-black" ...
+ */
+const ButtonComponent: FC<ButtonProps> = ({ ...props }) => {
   return (
-    <Button onClick={handleClick} variant={variant} size={size} type={type}>
-      {text}
-    </Button>
+    <div>
+      <Button {...props} />
+    </div>
   );
 };
 
-export default CustomButton;
+export default ButtonComponent;
