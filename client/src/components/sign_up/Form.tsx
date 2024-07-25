66b7fb92651baf4240c2cba6c9fe3973ac9a6084
eonduck2 <whdtnxd@gmail.com>
2024-07-25 14:29:16 +0900

    :package: 이종수: 회원 가입 form 태그 코드 (유효성 검증) 변경

--- client/src/components/sign_up/Form.tsx
+++ client/src/components/sign_up/Form.tsx
@@ -1,21 +1,6 @@
 import React, { useState } from "react";
-// import { useForm } from "react-hook-form";
-// import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
 import InputComponent from "src/components/Input";
 import SignUpInputs from "../../../static/sign-up/SignUpInputs";
-
-const passwordSchema = z
-  .string()
-  .min(8, { message: "비밀번호는 최소 8자, 16 이상이어야 합니다." })
-  .regex(/[a-z]/, { message: "비밀번호는 소문자를 포함해야 합니다." })
-  .regex(/[0-9]/, { message: "비밀번호는 숫자를 포함해야 합니다." })
-  .regex(
-    /^(?=.*[!@#$%^&*()_+{}\[\]:;"'<>,.?~\\/-])(?!.*[!@#$%^&*()_+{}\[\]:;"'<>,.?~\\/-].*[!@#$%^&*()_+{}\[\]:;"'<>,.?~\\/-]).*$/,
-    {
-      message: "비밀번호는 정확히 하나의 특수 문자를 포함해야 합니다.",
-    }
-  );
 
 const handleSubmit = (e: React.FormEvent) => {
   e.preventDefault();
@@ -23,20 +8,23 @@ const handleSubmit = (e: React.FormEvent) => {
 };
 
 const SignUpForm: React.FC = () => {
-  useState;
-
   return (
     <form id="sign-up-form" method="post" onSubmit={handleSubmit}>
       <div>
-        {SignUpInputs.map((itemArray, key) => (
-          <InputComponent
-            id={itemArray[1]}
-            name={itemArray[1]}
-            key={key}
-            placeholder={itemArray[0]}
-            type={itemArray[0]}
-          />
-        ))}
+        {SignUpInputs.map((itemArray, key) => {
+          return (
+            <>
+              <p>{itemArray[3]}</p>
+              <InputComponent
+                id={itemArray[1]}
+                name={itemArray[1]}
+                key={key}
+                placeholder={itemArray[2]}
+                type={itemArray[0]}
+              />
+            </>
+          );
+        })}
       </div>
       <button type="submit">ㅎㅇ</button>
     </form>
