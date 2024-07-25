4c4eb68869d7c48d992c6702640ab4932d75a194
moonhr <moonhr153@gmail.com>
2024-07-25 10:42:21 +0900

    :package:문혜림: 버튼 구조 수정

--- client/src/components/CustomButton.tsx
+++ client/src/components/CustomButton.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { Button } from "components/ui/button";
-import { TButton } from "../types/Tbutton.type";
+import { useNavigate } from "react-router-dom";
+import { CustomButtonProps } from "src/types/Tbutton.type";
 
-/**
- * @moonhr 24.07.25
- * @param { string } text 버튼 내용
- * @param { 'default' | 'destructive' | 'outline' | 'secondary' | 'ghost' | 'link' } variant 버튼 스타일
- * @param { 'default' | 'sm' | 'lg' | 'icon' } size 버튼 사이즈
- * @returns {JSX.Element} 버튼 컴포넌트
- */
-const CustomButton: React.FC<TButton> = ({ text, variant, size }) => {
+const CustomButton: React.FC<CustomButtonProps> = ({
+  to,
+  text,
+  variant,
+  size,
+}) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(to);
+  };
+
   return (
-    <Button variant={variant} size={size}>
+    <Button onClick={handleClick} variant={variant} size={size}>
       {text}
     </Button>
   );
