c4c7923e10644465c0332e2ef284a140adb15bab
문혜림 <moonhyerim@munhyelim-ui-MacBookPro.local>
2024-07-29 09:59:12 +0900

    :fire:문혜림: 테스트 더미, 코드 삭제

--- client/src/model/TableModel.ts
+++ client/src/model/TableModel.ts
@@ -1,5 +1,4 @@
 import { ProductDTO } from "../../../shared/DTO/products/product.dto";
-import { dummyProductData } from "./dummi";
 
 /**
  * @moonhr 24.07.28
@@ -7,12 +6,9 @@ import { dummyProductData } from "./dummi";
  * @returns json
  */
 export const fetchTableData = async (endpoint: string): Promise<ProductDTO> => {
-  // const response = await fetch(`$http://localhost:3001/${endpoint}`);
-  // if (!response.ok) {
-  //   throw new Error("Failed to fetch data");
-  // }
-  // return response.json();
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(dummyProductData), 1000); // 1초 후 더미 데이터를 반환
-  });
+  const response = await fetch(`$http://localhost:3001/${endpoint}`);
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return response.json();
 };
