a2a1834167d913a3efd7f5a989b2eb97416d1663
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-29 13:32:21 +0900

    :art:유호영:nav박스 완성

--- client/src/components/admin/navListBox.tsx
+++ client/src/components/admin/navListBox.tsx
@@ -1,45 +1,38 @@
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "components/ui/navigation-menu";
 import React from "react";
+import LinkButtonComponent from "../linkButtonComponent";
 
 const NavListBox: React.FC = () => {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>재고관리</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>
-              <button>재고조회</button>
-              <button>유통기한 관리</button>
-            </NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>매출관리</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>
-              <button>매출조회</button>
-              <button>매출순위</button>
-            </NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>회원관리</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>
-              <button>회원조회</button>
-            </NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <nav className="flex flex-col h-90% p-4">
+      <div className="text-lg font-semibold">재고 관리</div>
+      <ul className="pl-4 space-y-2">
+        <li>
+          <LinkButtonComponent href="/admin/stockInfo">재고 조회</LinkButtonComponent>
+        </li>
+        <li>
+          <LinkButtonComponent href="/admin/stockDate">유통기한 관리</LinkButtonComponent>
+        </li>
+      </ul>
+      <div className="text-lg font-semibold">매출 관리</div>
+      <ul className="pl-4 space-y-2">
+        <li>
+          <LinkButtonComponent href="/admin/salesInquiry">매출 조회</LinkButtonComponent>
+        </li>
+        <li>
+          <LinkButtonComponent href="/admin/salesRanking">매출 순위</LinkButtonComponent>
+        </li>
+      </ul>
+      <div className="text-lg font-semibold">회원 관리</div>
+      <ul className="pl-4 space-y-2">
+        <li>
+          <LinkButtonComponent href="/admin/memberInfo">회원 조회</LinkButtonComponent>
+        </li>
+      </ul>
+      <div className="mt-auto text-sm">
+        <LinkButtonComponent href="/myPage">마이페이지</LinkButtonComponent>
+        <LinkButtonComponent href="/logout">로그아웃</LinkButtonComponent>
+      </div>
+    </nav>
   );
 };
 
