26f421df8d8228c6a5ab6d8f68100963f4530d04
문혜림 <moonhyerim@munhyelim-ui-MacBookPro.local>
2024-07-29 09:56:13 +0900

    :sparkles:문혜림: 뷰모델을 통해 ui로 출력하는 테이블 뷰 작성
    재고에 대한 뷰를 출력함.

---
+++ client/src/components/table/ProductTable.tsx
@@ -0,0 +1,50 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@../../components/ui/table";
+import ButtonComponent from "../CustomButton";
+
+import { useTableViewModel } from "src/hook/useTableViewHook";
+
+export const ProductTable: React.FC = () => {
+  const { data, loading, error } = useTableViewModel("/");
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <Table>
+      <TableCaption>재고조회</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[200px]">재고명</TableHead>
+          <TableHead>수량</TableHead>
+          <TableHead>단가</TableHead>
+          <TableHead className="text-right">주문</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell className="font-medium">{data!.productName}</TableCell>
+          <TableCell>{data!.quantity}</TableCell>
+          <TableCell>{data!.unitPrice}</TableCell>
+          <TableCell className="text-right">
+            <ButtonComponent variant="default" type="submit">
+              주문하기
+            </ButtonComponent>
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  );
+};
