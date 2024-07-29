59a31cc13f82cb7ee0b373f65ba107b42970c46b
최유진 <yjyj0774@gmail.com>
2024-07-29 15:55:29 +0900

    :art: 최유진 : 사용자페이지 네비게이션 그림자 스타일 수정

--- client/src/components/UserNav.tsx
+++ client/src/components/UserNav.tsx
@@ -5,7 +5,7 @@ import FooterLinks from "./footerComponent";
 
 const UserNav = () => {
   return (
-    <div className="w-72 float-right shadow-md h-screen">
+    <div className="w-72 float-right shadow-xl h-screen">
       <LoginInfoComponent
         className="w-72 ml-6 mt-6 mb-20"
         email="rockcoders@kdt.com"
