34e6d589d80b709ce06c439761546d9854a58f77
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-29 10:24:28 +0900

    :art:유호영:nav component

--- client/src/components/admin/navListBox.tsx
+++ client/src/components/admin/navListBox.tsx
@@ -10,30 +10,40 @@ import React from "react";
 
 const NavListBox: React.FC = () => {
   return (
-    <NavigationMenu>
-      <NavigationMenuList className="flex-col">
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>재고관리</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>
+    <NavigationMenu className="h-90%">
+      <NavigationMenuList className="flex flex-col h-full">
+        <NavigationMenuItem className="relative mb-2">
+          <NavigationMenuTrigger className="text-lg py-3 px-6 bg-gray-100 rounded-md hover:bg-gray-200">
+            재고관리
+          </NavigationMenuTrigger>
+          <NavigationMenuContent className="absolute left-0 mt-2 bg-white shadow-lg rounded-md min-w-[200px] z-10">
+            <NavigationMenuLink className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
               <button>재고조회</button>
+            </NavigationMenuLink>
+            <NavigationMenuLink className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
               <button>유통기한 관리</button>
             </NavigationMenuLink>
           </NavigationMenuContent>
         </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>매출관리</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>
+        <NavigationMenuItem className="relative mb-2">
+          <NavigationMenuTrigger className="text-lg py-3 px-6 bg-gray-100 rounded-md hover:bg-gray-200">
+            매출관리
+          </NavigationMenuTrigger>
+          <NavigationMenuContent className="absolute left-0 mt-2 bg-white shadow-lg rounded-md min-w-[200px] z-10">
+            <NavigationMenuLink className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
               <button>매출조회</button>
+            </NavigationMenuLink>
+            <NavigationMenuLink className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
               <button>매출순위</button>
             </NavigationMenuLink>
           </NavigationMenuContent>
         </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>회원관리</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>
+        <NavigationMenuItem className="relative mb-2">
+          <NavigationMenuTrigger className="text-lg py-3 px-6 bg-gray-100 rounded-md hover:bg-gray-200">
+            회원관리
+          </NavigationMenuTrigger>
+          <NavigationMenuContent className="absolute left-0 mt-2 bg-white shadow-lg rounded-md min-w-[200px] z-10">
+            <NavigationMenuLink className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
               <button>회원조회</button>
             </NavigationMenuLink>
           </NavigationMenuContent>
