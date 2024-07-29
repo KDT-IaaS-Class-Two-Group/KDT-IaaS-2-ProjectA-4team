261ea47affce574c354cf60f80c0ce844ec192d8
문혜림 <moonhyerim@munhyelim-ui-MacBookPro.local>
2024-07-29 09:57:04 +0900

    :package:문혜림: 테스트를 위한 수정

--- shared/DTO/products/product.dto.ts
+++ shared/DTO/products/product.dto.ts
@@ -1,28 +1,37 @@
-import IProduct from '../../../db/products/product.interface';
-import { BaseDTO } from './base.dto';
+// import IProduct from '../../../db/products/product.interface';
+// import { BaseDTO } from './base.dto';
 
-export class ProductDTO extends BaseDTO {
-  public productID: number;
-  public productName: string;
-  public unitPrice: number;
-  public quantity: number;
-  public restockDate?: Date;
-  public expirationDate: Date;
+// export class ProductDTO extends BaseDTO {
+//   public productID: number;
+//   public productName: string;
+//   public unitPrice: number;
+//   public quantity: number;
+//   public restockDate?: Date;
+//   public expirationDate: Date;
 
-  constructor(product: IProduct) {
-    super(
-      product.productID,
-      product.productName,
-      product.unitPrice,
-      product.quantity,
-      product.restockData,
-      product.expirationDate
-    );
-    this.productID = product.productID;
-    this.productName = product.productName;
-    this.unitPrice = product.unitPrice;
-    this.quantity = product.quantity;
-    this.restockDate = product.restockData;
-    this.expirationDate = product.expirationDate;
-  }
+//   constructor(product: IProduct) {
+//     super(
+//       product.productID,
+//       product.productName,
+//       product.unitPrice,
+//       product.quantity,
+//       product.restockData,
+//       product.expirationDate
+//     );
+//     this.productID = product.productID;
+//     this.productName = product.productName;
+//     this.unitPrice = product.unitPrice;
+//     this.quantity = product.quantity;
+//     this.restockDate = product.restockData;
+//     this.expirationDate = product.expirationDate;
+//   }
+// }
+
+export interface ProductDTO {
+  productID: number;
+  productName: string;
+  unitPrice: number;
+  quantity: number;
+  restockDate?: Date;
+  expirationDate: Date;
 }
