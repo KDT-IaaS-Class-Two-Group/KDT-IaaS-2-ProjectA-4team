0b6139a120d10aa295f4bc16efa914cff3d11ce6
최유진 <yjyj0774@gmail.com>
2024-07-29 15:04:59 +0900

    :wrench: 최유진 : 사용자페이지 네비게이션에 들어가는 컴포넌트들 불러오기

--- client/src/components/UserNav.tsx
+++ client/src/components/UserNav.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import LoginInfoComponent from "./LoginInfo";
+import Cart from "./Cart";
+import FooterLinks from "./footerComponent";
 
 const UserNav = () => {
-  return <div></div>;
+  return (
+    <div>
+      <LoginInfoComponent email="rockcoders@kdt.com" />
+      <Cart />
+      <FooterLinks />
+    </div>
+  );
 };
 
 export default UserNav;
