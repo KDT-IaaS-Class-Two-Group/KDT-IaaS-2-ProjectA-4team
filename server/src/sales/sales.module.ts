4fb7f250003f58856551424e41efef20547497d7
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-29 17:35:23 +0900

    :heavy_plus_sign:유호영:Sales관리 파일 모듈화 및 DB모델 생성

---
+++ server/src/sales/sales.module.ts
@@ -0,0 +1,13 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import saleSchema from "@db/sale/schema/sale.schema";
+import { SaleService } from "./sales.service";
+import { SaleController } from "./sales.controller";
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'Sale', schema: saleSchema }])],
+  providers: [SaleService],
+  controllers: [SaleController],
+})
+
+export class SaleModule {}
