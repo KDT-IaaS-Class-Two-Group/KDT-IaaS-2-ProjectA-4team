4c3b76b8ef097cb2f3aec4c2b1ac81f3f71ed46a
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-29 15:43:44 +0900

    :art:유호영:salesInquiry component

--- client/src/pages/admin/salesInquiry.tsx
+++ client/src/pages/admin/salesInquiry.tsx
@@ -1,1 +1,12 @@
-// * 매출 조회
+import React from "react";
+import AdminNav from "src/components/admin/adminNav";
+
+const SalesInquiry:React.FC = () => {
+  return (
+    <div className="flex flex-row">
+      <AdminNav />
+    </div>
+  );
+}
+
+export default SalesInquiry;
