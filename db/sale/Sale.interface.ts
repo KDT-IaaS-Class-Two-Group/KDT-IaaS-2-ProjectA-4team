26d83a07926d0e27287d17cecc333c118d453b38
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-30 11:02:44 +0900

    :heavy_minus_sign:유호영:sale interface document분리

--- db/sale/Sale.interface.ts
+++ db/sale/Sale.interface.ts
@@ -1,4 +1,3 @@
-import { Document } from "mongoose";
 import IMemberID from "../../shared/DTO/sale/interface/MemberID.interface";
 import ISaleDate from "../../shared/DTO/sale/interface/SaleDate.interface";
 import ISaleID from "../../shared/DTO/sale/interface/SaleID.interface";
@@ -9,6 +8,6 @@ import ITotalPrice from "../../shared/DTO/sale/interface/TotalPrice.interface";
  * @crystal23733 24.07.22
  * * sale interface
  */
-export default interface ISale extends IMemberID, ISaleDate, ISaleID, ITotalPrice, Document{
+export default interface ISale extends IMemberID, ISaleDate, ISaleID, ITotalPrice{
   products : ISaleProducts[];
 }
