09a1d02733f74c08c3521c59f6f267ed4a7e030a
최유진 <yjyj0774@gmail.com>
2024-07-30 11:10:56 +0900

    :package: 최유진 : 조건부 렌더링시킬 제품들 컴포넌트화

--- client/src/pages/UserPage.tsx
+++ client/src/pages/UserPage.tsx
@@ -1,8 +1,8 @@
 import React, { FC, useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import CardComponent from "src/components/Card";
 import UserNav from "src/components/users/UserNav";
 import LinkButtonComponent from "src/components/linkButtonComponent";
+import MenuItems from "src/components/users/MenuItems";
 
 /**
  * @yuxincxoi 24.07.25
@@ -20,52 +20,7 @@ const UserPage: FC = () => {
       setSelectCategory(category);
     }
   }, [router.query.category]);
-  const menuItems = () => {
-    switch (selectCategory) {
-      case "bread":
-        return (
-          <>
-            <CardComponent title="화이트" content={4000} />
-            <CardComponent title="허니오트" content={4000} />
-            <CardComponent title="플랫 브레드" content={4000} />
-          </>
-        );
-      case "patty":
-        return (
-          <>
-            <CardComponent title="게살 패티" content={5000} />
-            <CardComponent title="징징이다리 패티" content={5000} />
-            <CardComponent title="집게사장 손 패티" content={5000} />
-          </>
-        );
-      case "source":
-        return (
-          <>
-            <CardComponent title="토마토 소스" content={2000} />
-            <CardComponent title="바베큐 소스" content={2500} />
-            <CardComponent title="마요네즈 소스" content={1800} />
-          </>
-        );
-      case "side":
-        return (
-          <>
-            <CardComponent title="감자튀김" content={1500} />
-            <CardComponent title="치킨 너겟" content={2000} />
-            <CardComponent title="어니언 링" content={1800} />
-          </>
-        );
-      case "drink":
-        return (
-          <>
-            <CardComponent title="콜라" content={1200} />
-            <CardComponent title="사이다" content={1200} />
-            <CardComponent title="주스" content={1500} />
-          </>
-        );
-      default:
-        return null;
-    }
-  };
+
   return (
     <div>
       <div>
@@ -98,7 +53,7 @@ const UserPage: FC = () => {
         </LinkButtonComponent>
       </div>
       <div className="absolute flex m-10 justify-between w-[1100px]">
-        {menuItems()}
+        <MenuItems selectCategory={selectCategory} />
       </div>
       <UserNav />
     </div>
