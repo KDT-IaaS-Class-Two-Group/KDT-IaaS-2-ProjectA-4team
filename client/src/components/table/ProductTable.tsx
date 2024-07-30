ae7de975b55b9dd09b2f84f03ca4f04821dac027
문혜림 <moonhyerim@munhyelim-ui-MacBookPro.local>
2024-07-30 11:38:54 +0900

    :package:문혜림: 재고 테이블(view) 두개의 훅을 사용하도록 수정
    제품을 불러오는 ProductUseTableHook.
    제품을 저장하는 useOrderMadalHook.을 사용함.
    이는 두개의 뷰모델을 참조하고 있음을 의미함.

--- client/src/components/table/ProductTable.tsx
+++ client/src/components/table/ProductTable.tsx
@@ -8,11 +8,22 @@ import {
   TableRow,
 } from "@../../components/ui/table";
 import ButtonComponent from "../CustomButton";
-
 import { ProductUseTableHook } from "src/hooks/ProductUseTableHook";
+import OrderModal from "../modal/OrderModal";
+import React from "react";
+import useOrderModalHook from "src/hooks/useOrderModalHook";
 
 export const ProductTable: React.FC = () => {
   const { data, loading, error } = ProductUseTableHook();
+  const {
+    modalOpen,
+    selectedProduct,
+    quantity,
+    setQuantity,
+    openModal,
+    closeModal,
+    saveOrder,
+  } = useOrderModalHook();
 
   if (loading) {
     return <div>Loading...</div>;
@@ -23,31 +34,48 @@ export const ProductTable: React.FC = () => {
   }
 
   return (
-    <Table>
-      <TableCaption>재고조회</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[200px]">재고명</TableHead>
-          <TableHead>수량</TableHead>
-          <TableHead>단가</TableHead>
-          <TableHead className="text-right">주문</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {/* 데이터 수만큼 열 생성 */}
-        {data.map((row) => (
-          <TableRow key={row.productID}>
-            <TableCell className="font-medium">{row!.productName}</TableCell>
-            <TableCell>{row!.quantity}</TableCell>
-            <TableCell>{row!.unitPrice}</TableCell>
-            <TableCell className="text-right">
-              <ButtonComponent variant="default" type="submit">
-                주문하기
-              </ButtonComponent>
-            </TableCell>
+    <>
+      <Table>
+        <TableCaption>재고조회</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[200px]">재고명</TableHead>
+            <TableHead>수량</TableHead>
+            <TableHead>단가</TableHead>
+            <TableHead className="text-right">주문</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {/* 데이터 수만큼 열 생성 */}
+          {data.map((row) => (
+            <TableRow key={row.productID}>
+              <TableCell className="font-medium">{row!.productName}</TableCell>
+              <TableCell>{row!.quantity}</TableCell>
+              <TableCell>{row!.unitPrice}</TableCell>
+              <TableCell className="text-right">
+                <ButtonComponent
+                  variant="default"
+                  type="button"
+                  onClick={() => openModal(row)}
+                >
+                  주문하기
+                </ButtonComponent>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+      {modalOpen && (
+        <OrderModal
+          isOpen={modalOpen}
+          product={selectedProduct}
+          quantity={quantity}
+          setQuantity={setQuantity}
+          onSave={saveOrder}
+          onClose={closeModal}
+        />
+      )}
+    </>
   );
 };
