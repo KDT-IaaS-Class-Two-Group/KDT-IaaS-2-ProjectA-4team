f36919e51f7c23e2503f03397b5723ef6042f190
moonhr <moonhr153@gmail.com>
2024-07-30 17:18:31 +0900

    :sparkles:문혜림:수정모달창 생성

---
+++ client/src/components/modal/UpdateModal.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "../../../components/ui/dialog";
+import InputComponent from "../Input";
+import ButtonComponent from "../CustomButton";
+import { ProductDTO } from "@shared/DTO/products/product.dto";
+
+const UpdateModal = () => {
+  return <Modal onClose={} title="" content="" />;
+};
+
+export default UpdateModal;
