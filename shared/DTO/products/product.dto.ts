6310eaee403ad70084c42e81e852406de4f70d3c
문혜림 <moonhyerim@munhyelim-ui-MacBookPro.local>
2024-07-30 16:30:05 +0900

    :package:문혜림: productDTO 정의

--- shared/DTO/products/product.dto.ts
+++ shared/DTO/products/product.dto.ts
@@ -1,8 +1,9 @@
-import IProduct from '../../../db/products/product.interface';
-import { BaseDTO } from './base.dto';
+import IProduct from "../../../db/products/product.interface";
+import { BaseDTO } from "./base.dto";
 
 export class ProductDTO extends BaseDTO {
   public productID: number;
+  public productCategory: string;
   public productName: string;
   public unitPrice: number;
   public quantity: number;
@@ -12,17 +13,19 @@ export class ProductDTO extends BaseDTO {
   constructor(product: IProduct) {
     super(
       product.productID,
+      product.productCategory,
       product.productName,
       product.unitPrice,
       product.quantity,
-      product.restockData,
+      product.restockDate,
       product.expirationDate
     );
     this.productID = product.productID;
+    this.productCategory = product.productCategory;
     this.productName = product.productName;
     this.unitPrice = product.unitPrice;
     this.quantity = product.quantity;
-    this.restockDate = product.restockData;
+    this.restockDate = product.restockDate;
     this.expirationDate = product.expirationDate;
   }
 }
