e8c470259d0131dfe412210bba4c0b499e62515e
eonduck2 <whdtnxd@gmail.com>
2024-07-30 15:40:34 +0900

    :package: 이종수: 회원 관리 페이지 코드 일부 수정

--- client/src/pages/admin/Member-Info.tsx
+++ client/src/pages/admin/Member-Info.tsx
@@ -1,20 +1,32 @@
+import { useState } from "react";
 import AdminNav from "src/components/admin/adminNav";
+import Modal from "src/components/modal/Modal";
 import MemberInfoTable from "src/components/table/MemberInfoTable";
 
 export default () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleClose = () => {
+    setModalOpen(false);
+  };
+
   return (
     <>
+      {isModalOpen && (
+        <Modal title="테스트" content="ㅎㅇ" onClose={handleClose} />
+      )}
       <div className="flex">
-        <AdminNav></AdminNav>
+        <AdminNav />
         <MemberInfoTable
-          buttonValue="정보 변경"
-          head={["이름", "이메일", "정보"]}
+          buttonValue="권한 변경"
+          head={["회원 번호", "이름", "이메일", "권한"]}
           data={[
-            { 회원: "이종수", 이메일: "qwewq@qwewqe123" },
-            { 회원: "이종수", 이메일: "qwewq@qwewqe123" },
+            { id: "1000", 회원: "이종수", 이메일: "qwewq@qwewqe123" },
+            { id: "2000", 회원: "이종수", 이메일: "qwewq@qwewqe123" },
           ]}
-        ></MemberInfoTable>
+        />
       </div>
+      <button onClick={() => setModalOpen(true)}>open</button>
     </>
   );
 };
