85f29e1eb9c78b4e55056d9d7125c75fa7fdb4f7
문혜림 <moonhyerim@munhyelim-ui-MacBookPro.local>
2024-07-30 16:29:45 +0900

    :sparkles:문혜림: product 인터페이스 정의

--- db/products/product.interface.ts
+++ db/products/product.interface.ts
@@ -1,8 +1,9 @@
 import IExpirationDate from "../../shared/DTO/products/interface/ExpirationDate.interface";
+import IProductCategory from "../../shared/DTO/products/interface/ProductCategory.interface";
 import IProductID from "../../shared/DTO/products/interface/ProductID.interface";
 import IProductName from "../../shared/DTO/products/interface/ProductName.interface";
 import IQuantity from "../../shared/DTO/products/interface/Quantity.interface";
-import IRestockData from "../../shared/DTO/products/interface/RestockData.interface";
+import IRestockDate from "../../shared/DTO/products/interface/RestockData.interface";
 import IUnitPrice from "../../shared/DTO/products/interface/UnitPrice.interface";
 
 /**
@@ -10,6 +11,11 @@ import IUnitPrice from "../../shared/DTO/products/interface/UnitPrice.interface"
  * * product interface
  */
 
-export default interface IProduct extends IProductID, IProductName, IUnitPrice, IQuantity, IRestockData, IExpirationDate{
-
-}
+export default interface IProduct
+  extends IProductID,
+    IProductName,
+    IUnitPrice,
+    IQuantity,
+    IRestockDate,
+    IExpirationDate,
+    IProductCategory {}
