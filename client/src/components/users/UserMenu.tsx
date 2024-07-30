aa4335b3915d5f79b803fd9d027828437534f8bf
최유진 <yjyj0774@gmail.com>
2024-07-30 10:08:51 +0900

    :wrench: 최유진 : 사용자페이지 메뉴 컴포넌트 파일 생성

---
+++ client/src/components/users/UserMenu.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+/**
+ * @yuxincxoi 24.07.30
+ * 사용자페이지의 구매할 제품 카테고리를 나타내는 메뉴 컴포넌트
+ * @returns {JSXElement}
+ */
+
+const UserMenu = () => {
+  return <div></div>;
+};
+
+export default UserMenu;
