58b37815a384bf32ab3d2ad978aa4f8f5e094c13
최유진 <yjyj0774@gmail.com>
2024-07-30 11:39:33 +0900

    :package: 최유진 : 제품 카테고리를 나타내는 메뉴 컴포넌트화

--- client/src/pages/UserPage.tsx
+++ client/src/pages/UserPage.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import UserNav from "src/components/users/UserNav";
-import LinkButtonComponent from "src/components/linkButtonComponent";
+import UserMenu from "src/components/users/UserMenu";
 import MenuItems from "src/components/users/MenuItems";
 
 /**
@@ -23,35 +23,7 @@ const UserPage: FC = () => {
 
   return (
     <div>
-      <div>
-        <LinkButtonComponent
-          href="#"
-          onClick={() => setSelectCategory("bread")}
-        >
-          빵
-        </LinkButtonComponent>
-        <LinkButtonComponent
-          href="#"
-          onClick={() => setSelectCategory("patty")}
-        >
-          패티
-        </LinkButtonComponent>
-        <LinkButtonComponent
-          href="#"
-          onClick={() => setSelectCategory("source")}
-        >
-          소스
-        </LinkButtonComponent>
-        <LinkButtonComponent href="#" onClick={() => setSelectCategory("side")}>
-          사이드
-        </LinkButtonComponent>
-        <LinkButtonComponent
-          href="#"
-          onClick={() => setSelectCategory("drink")}
-        >
-          음료
-        </LinkButtonComponent>
-      </div>
+      <UserMenu setSelectCategory={setSelectCategory} />
       <div className="absolute flex m-10 justify-between w-[1100px]">
         <MenuItems selectCategory={selectCategory} />
       </div>
