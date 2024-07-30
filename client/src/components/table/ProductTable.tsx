ecc68eba37935c2efe00c9925914671deab4903b
moonhr <moonhr153@gmail.com>
2024-07-30 17:18:40 +0900

    :package:문혜림: 레이아웃 수정

--- client/src/components/table/ProductTable.tsx
+++ client/src/components/table/ProductTable.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Table,
   TableBody,
@@ -8,22 +9,13 @@ import {
   TableRow,
 } from "@../../components/ui/table";
 import ButtonComponent from "../CustomButton";
+// import OrderModal from "../modal/OrderModal";
+// import UpdateModal from "../modal/updateModal";
 import { ProductUseTableHook } from "src/hooks/ProductUseTableHook";
-import OrderModal from "../modal/OrderModal";
-import React from "react";
-import useOrderModalHook from "src/hooks/useOrderModalHook";
+// import useOrderModalHook from "src/hooks/useOrderModalHook";
 
 export const ProductTable: React.FC = () => {
   const { data, loading, error } = ProductUseTableHook();
-  const {
-    modalOpen,
-    selectedProduct,
-    quantity,
-    setQuantity,
-    openModal,
-    closeModal,
-    saveOrder,
-  } = useOrderModalHook();
 
   if (loading) {
     return <div>Loading...</div>;
@@ -39,43 +31,44 @@ export const ProductTable: React.FC = () => {
         <TableCaption>재고조회</TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[200px]">재고명</TableHead>
+            <TableHead className="w-[200px]">카테고리</TableHead>
+            <TableHead>재품명</TableHead>
             <TableHead>수량</TableHead>
             <TableHead>단가</TableHead>
-            <TableHead className="text-right">주문</TableHead>
+            <TableHead>발주</TableHead>
+            <TableHead className="text-right">수정</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {/* 데이터 수만큼 열 생성 */}
           {data.map((row) => (
             <TableRow key={row.productID}>
-              <TableCell className="font-medium">{row!.productName}</TableCell>
+              <TableCell className="font-medium">
+                {row!.productCategory}
+              </TableCell>
+              <TableCell>{row!.productName}</TableCell>
               <TableCell>{row!.quantity}</TableCell>
               <TableCell>{row!.unitPrice}</TableCell>
               <TableCell className="text-right">
                 <ButtonComponent
                   variant="default"
                   type="button"
-                  onClick={() => openModal(row)}
+                  // onClick={() => openModal(row)}
                 >
                   주문하기
+                </ButtonComponent>
+                <ButtonComponent
+                  variant="default"
+                  type="button"
+                  // onClick={() => openModal(row)}
+                >
+                  수정하기
                 </ButtonComponent>
               </TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
-
-      {modalOpen && (
-        <OrderModal
-          isOpen={modalOpen}
-          product={selectedProduct}
-          quantity={quantity}
-          setQuantity={setQuantity}
-          onSave={saveOrder}
-          onClose={closeModal}
-        />
-      )}
     </>
   );
 };
