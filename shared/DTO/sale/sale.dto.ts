2998e78607b228cf1f5361f30558291680b6aadb
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-30 13:06:36 +0900

    :heavy_minus_sign:유호영__
    Sale에 대한 inteface products : ISaleProducts[]; -> products : ISaleProducts;변경

--- shared/DTO/sale/sale.dto.ts
+++ shared/DTO/sale/sale.dto.ts
@@ -5,14 +5,14 @@ import { SaleProductDTO } from "./saleProduct.dto";
 export class SaleDTO implements ISale {
   public saleID: number;
   public memberID: string;
-  public products: SaleProductDTO[];
+  public products: SaleProductDTO; // 단일 객체로 변경
   public saleDate: Date;
   public totalPrice: number;
 
   constructor(sale: ISale) {
     this.saleID = sale.saleID;
     this.memberID = sale.memberID;
-    this.products = sale.products.map(product => new SaleProductDTO(product));
+    this.products = new SaleProductDTO(sale.products); // 단일 객체로 처리
     this.saleDate = sale.saleDate;
     this.totalPrice = sale.totalPrice;
   }
@@ -21,7 +21,7 @@ export class SaleDTO implements ISale {
     return {
       saleID: this.saleID,
       memberID: this.memberID,
-      products: this.products.map(product => product.toJSON()),
+      products: this.products.toJSON(), // 단일 객체의 toJSON() 호출
       saleDate: this.saleDate,
       totalPrice: this.totalPrice
     };
