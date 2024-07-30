b103575163d3f993529aeaf07a5243f0367e0900
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-30 10:37:58 +0900

    :sparkles:유호영:server측 cors기능 추가

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin : "http://localhost:3000",
+    methods: "GET, POST, PUT, PATCH, DELETE",
+    allowedHeaders: "Content-type, Authorization",
+    credentials: true,
+  })
   await app.listen(3001);
   console.log(`http://localhost:3001`);
 }
