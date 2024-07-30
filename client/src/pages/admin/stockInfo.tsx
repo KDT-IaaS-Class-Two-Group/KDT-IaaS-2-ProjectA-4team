68c2ec09fa8e07756b59d744f099c23f933f7bcb
문혜림 <moonhyerim@munhyelim-ui-MacBookPro.local>
2024-07-30 12:16:42 +0900

    :sparkles:문혜림:관리자 페이지 레이아웃 제작
    재고관리 기준으로 작성됨. 각 페이지에 맞는 테이블만 변경하여 활용.

--- client/src/pages/admin/stockInfo.tsx
+++ client/src/pages/admin/stockInfo.tsx
@@ -1,1 +1,22 @@
 // * 재고 조회
+import NavListBox from "src/components/admin/navListBox";
+import { ProductTable } from "src/components/table/ProductTable";
+import LoginInfoComponent from "src/components/LoginInfo";
+
+const StockInfo = () => {
+  return (
+    <>
+      <div className="grid w-screen h-screen gap-4 p-3 grid-cols-custom-30-70">
+        <NavListBox />
+        <div className="flex flex-col gap-4">
+          <div className="flex items-center justify-start w-auto h-16 p-3 border border-black border-solid">
+            <LoginInfoComponent email=""></LoginInfoComponent>
+          </div>
+          {/* 각 페이지에 맞는 테이블 배치 */}
+          <ProductTable></ProductTable>
+        </div>
+      </div>
+    </>
+  );
+};
+export default StockInfo;
