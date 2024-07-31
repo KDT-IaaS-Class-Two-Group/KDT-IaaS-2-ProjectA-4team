9e4708f48c705a0dc263b49a3cb6302a64c1e8db
최유진 <yjyj0774@gmail.com>
2024-07-31 21:38:26 +0900

    :fire: 최유진 : 필요없는 코드 삭제

--- client/src/pages/UserPage.tsx
+++ client/src/pages/UserPage.tsx
@@ -30,11 +30,12 @@ const UserPage: FC = () => {
     setCartItems((prevItems) => [...prevItems, { menu, unitPrice }]);
   };
 
-  const [dataFromChild, setDataFromChild] = useState<string>("parent");
-  const handleDataFromChild = (data: string) => {
-    setDataFromChild(data);
-    console.log(dataFromChild);
-  };
+  // const [dataFromChild, setDataFromChild] = useState<string>("parent");
+
+  // const handleDataFromChild = (data: string) => {
+  //   setDataFromChild(data);
+  //   console.log(dataFromChild);
+  // };
 
   return (
     <div>
@@ -49,7 +50,7 @@ const UserPage: FC = () => {
           className="w-72 ml-6 mt-6 mb-20"
           email="rockcoders@kdt.com"
         />
-        <Cart items={cartItems} eventHandle={handleDataFromChild} />
+        <Cart items={cartItems} />
         <FooterLinks className="w-72 mt-20 mx-6" />
       </div>
     </div>
