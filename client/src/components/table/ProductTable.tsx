15d75238204e6b13122068b68009403704664db4
moonhr <moonhr153@gmail.com>
2024-07-31 12:07:13 +0900

    :package:문혜림: 테이블에서 모달창이 반응하도록 수정
    버튼클릭 시 모달창 활성화
    모달창의 변경사항을 저장하고  데이터베이스에 요청해서 화면이 리로드 되도록 추가앟

--- client/src/components/table/ProductTable.tsx
+++ client/src/components/table/ProductTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Table,
   TableBody,
@@ -9,9 +9,39 @@ import {
 } from "@../../components/ui/table";
 import ButtonComponent from "../CustomButton";
 import { ProductUseTableHook } from "src/hooks/ProductUseTableHook";
+import UpdateModal from "../modal/UpdateModal";
+import { ProductDTO } from "@shared/DTO/products/product.dto";
+import { updateProductUpdate } from "src/model/productFetchUpdate";
 
+/**
+ * @moonhr 24.07.31
+ * @returns 재고 테이블
+ */
 const ProductTable: React.FC = () => {
-  const { data, loading, error } = ProductUseTableHook();
+  const { data, loading, error, refetch } = ProductUseTableHook();
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [selectedProduct, setSelectedProduct] = useState<ProductDTO | null>(
+    null,
+  );
+
+  const openModal = (product: ProductDTO) => {
+    setSelectedProduct(product);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+    setSelectedProduct(null);
+  };
+
+  const handleSave = async (updatedProduct: ProductDTO) => {
+    try {
+      await updateProductUpdate(updatedProduct);
+      refetch();
+    } catch (err) {
+      console.error("Failed to fetch updated products:", err);
+    }
+  };
 
   if (loading) {
     return <div>Loading...</div>;
@@ -22,44 +52,58 @@ const ProductTable: React.FC = () => {
   }
 
   return (
-    <div className="flex flex-col items-end justify-center gap-4">
-      <ButtonComponent variant="secondary">메뉴 추가</ButtonComponent>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">카테고리</TableHead>
-            <TableHead>재품명</TableHead>
-            <TableHead>수량</TableHead>
-            <TableHead>단가</TableHead>
-            <TableHead className="text-center">발주</TableHead>
-            <TableHead className="text-center">수정</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {/* 데이터 수만큼 열 생성 */}
-          {data.map((row) => (
-            <TableRow key={row.productID}>
-              <TableCell className="font-medium">
-                {row!.productCategory}
-              </TableCell>
-              <TableCell>{row!.productName}</TableCell>
-              <TableCell>{row!.quantity}</TableCell>
-              <TableCell>{row!.unitPrice}</TableCell>
-              <TableCell className="text-center">
-                <ButtonComponent variant="default" type="button">
-                  발주하기
-                </ButtonComponent>
-              </TableCell>
-              <TableCell className="text-center">
-                <ButtonComponent variant="default" type="button">
-                  수정하기
-                </ButtonComponent>
-              </TableCell>
+    <>
+      <div className="flex flex-col items-end justify-center gap-4">
+        <ButtonComponent variant="secondary">메뉴 추가</ButtonComponent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">카테고리</TableHead>
+              <TableHead>재품명</TableHead>
+              <TableHead>수량</TableHead>
+              <TableHead>판매가</TableHead>
+              <TableHead className="text-center">발주</TableHead>
+              <TableHead className="text-center">수정</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </div>
+          </TableHeader>
+          <TableBody>
+            {/* 데이터 수만큼 열 생성 */}
+            {data.map((row) => (
+              <TableRow key={row.productID}>
+                <TableCell className="font-medium">
+                  {row!.productCategory}
+                </TableCell>
+                <TableCell>{row!.productName}</TableCell>
+                <TableCell>{row!.quantity}</TableCell>
+                <TableCell>{row!.unitPrice}</TableCell>
+                <TableCell className="text-center">
+                  <ButtonComponent variant="default" type="button">
+                    발주하기
+                  </ButtonComponent>
+                </TableCell>
+                <TableCell className="text-center">
+                  <ButtonComponent
+                    variant="default"
+                    type="button"
+                    onClick={() => openModal(row)}
+                  >
+                    수정하기
+                  </ButtonComponent>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+      {modalOpen && selectedProduct && (
+        <UpdateModal
+          isOpen={modalOpen}
+          product={selectedProduct}
+          onClose={closeModal}
+          onSave={handleSave}
+        />
+      )}
+    </>
   );
 };
 
