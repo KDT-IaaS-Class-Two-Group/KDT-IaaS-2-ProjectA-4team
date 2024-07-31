ea1fdc79a63ad478e2b4c90ff73587ec7adf442f
최유진 <yjyj0774@gmail.com>
2024-07-31 11:50:27 +0900

    :package: 최유진 : 사용자 네비게이션 컴포넌트 분해

--- client/src/pages/UserPage.tsx
+++ client/src/pages/UserPage.tsx
@@ -1,8 +1,10 @@
 import React, { FC, useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import UserNav from "src/components/users/UserNav";
 import UserMenu from "src/components/users/UserMenu";
 import MenuItems from "src/components/users/MenuItems";
+import LoginInfoComponent from "src/components/LoginInfo";
+import Cart from "src/components/users/Cart";
+import FooterLinks from "src/components/footerComponent";
 
 /**
  * @yuxincxoi 24.07.25
@@ -36,8 +38,14 @@ const UserPage: FC = () => {
         selectCategory={selectCategory}
         onAddToCart={handleAddToCart}
       />
-      <UserNav />
-      {/* <UserNav /> */}
+      <div className="w-72 shadow-xl h-screen fixed top-0 right-0">
+        <LoginInfoComponent
+          className="w-72 ml-6 mt-6 mb-20"
+          email="rockcoders@kdt.com"
+        />
+        <Cart items={cartItems} />
+        <FooterLinks className="w-72 mt-24 mx-6" />
+      </div>
     </div>
   );
 };
