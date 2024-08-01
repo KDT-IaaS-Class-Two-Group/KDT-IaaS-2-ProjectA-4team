9b21b24d999e27dc0f4713c9f203944212add958
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-01 17:34:03 +0900

    :bulb:유호영:주석처리 추가

--- client/src/hooks/orderHook.ts
+++ client/src/hooks/orderHook.ts
@@ -1,6 +1,13 @@
-import { useEffect, useState } from "react";
+import { JSXElementConstructor, useEffect, useState } from "react";
 import orderFetch from "src/model/orderFetch";
 
+/**
+ * @crystal23733 24.08.01
+ * @param {string} name 더미데이터
+ * @returns {object} 
+ * - orderDetails : 주문 데이터
+ * - error : 에러 메세지
+ */
 const useOrderHook = (name: string) => {
   const [orderDetails, setOrderDetails] = useState<any[]>([]);
   const [error, setError] = useState<string | null>(null);
--- client/src/pages/myPage.tsx
+++ client/src/pages/myPage.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { JSXElementConstructor } from "react";
 import LinkButtonComponent from "src/components/linkButtonComponent";
 import LoginInfoComponent from "src/components/LoginInfo";
 import MyPageFormComponent from "src/components/myPageFormComponent";
 import OrderDetails from "src/components/orderDetails";
 import useOrderHook from "src/hooks/orderHook";
 
+/**
+ * @crystal23733 24.08.01
+ * @returns {JSXElement} 
+ */
 const MyPage: React.FC = () => {
   const memberName = "유호영";
   const {orderDetails, error} = useOrderHook(memberName);
