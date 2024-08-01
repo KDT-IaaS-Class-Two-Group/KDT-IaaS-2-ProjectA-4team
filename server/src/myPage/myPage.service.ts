a7a7ab1bc23afce149e7aaedb920144e1ba4a757
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-01 11:10:33 +0900

    :sparkles:유호영:비밀번호 변경 서비스 로직 완성

--- server/src/myPage/myPage.service.ts
+++ server/src/myPage/myPage.service.ts
@@ -8,18 +8,20 @@ import IMember from '@db/members/member.interface';
  * @crystal23733 24.07.31
  * * 비밀번호 변경 로직
  */
-export class ChangePasswordService {
-  constructor(@InjectModel('Member') private readonly memberModel:Model<IMember>) {}
-  async changePassword(memberID:string, oldPassword:string, newPassword:string):Promise<any> {
-    const member = await this.memberModel.findById(memberID).exec();
-    if(!member){
-      throw new NotFoundException("회원이 존재하지 않습니다.");
+@Injectable()
+export class PasswordService {
+  constructor(@InjectModel('Member') private readonly memberModel: Model<IMember>) {}
+
+  async changePassword(name: string, oldPassword: string, newPassword: string): Promise<any> {
+    const member = await this.memberModel.findOne({name});
+    if (!member) {
+      throw new NotFoundException('사용자를 찾을 수 없습니다.');
     }
-    if(member.password !== oldPassword){
-      throw new UnauthorizedException("기존 비밀번호가 일치하지 않습니다.");
+    if (member.password !== oldPassword) {
+      throw new Error('이전 비밀번호가 일치하지 않습니다.');
     }
     member.password = newPassword;
     await member.save();
-    return {message : '비밀번호를 성공적으로 변경하였습니다!'};
+    return { message: '비밀번호가 성공적으로 변경되었습니다.' };
   }
 }
