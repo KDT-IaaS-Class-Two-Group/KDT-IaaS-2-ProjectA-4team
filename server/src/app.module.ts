5009cc696eb37d912eaf655c2ab11060ece26e00
moonhr <moonhr153@gmail.com>
2024-08-01 18:07:50 +0900

    :pakcage:문혜림: app모듈에 stock설정 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SaleModule } from './sales/sales.module';
+import { StockModule } from './stock/stock.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot("mongodb://localhost:27017/rockcodersERP"),
-    SaleModule
+    MongooseModule.forRoot('mongodb://localhost:27017/rockcodersERP'),
+    SaleModule,
+    StockModule,
   ],
   controllers: [AppController],
   providers: [AppService],
