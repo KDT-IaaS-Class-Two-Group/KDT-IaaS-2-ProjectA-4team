ca93149202f1a5f10c467862517d811b673e3de4
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-01 16:22:36 +0900

    :package:유호영:프리티어 코드 정리

--- client/src/model/orderFetch.ts
+++ client/src/model/orderFetch.ts
@@ -3,16 +3,16 @@
  * todo 유저의 정보는 추후 토큰으로 대체 필요
  * @param {string} name 유저 더미데이터
  */
-export default async (name:string) => {
+export default async (name: string) => {
   try {
     const response = await fetch(`http://localhost:3001/orders/${name}`);
     const responseData = await response.json();
-    if(!response.ok){
+    if (!response.ok) {
       throw new Error(responseData.message || "주문내역 조회 실패");
     }
     return responseData;
-  } catch(error) {
+  } catch (error) {
     console.error("주문내역 조회 중 클라이언트 오류", error);
     throw error;
   }
-}
+};
