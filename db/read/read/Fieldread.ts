86e7667532d1f7a931c7c5152f4761f4b10440be
jojayeon <jojayeon6152@gmail.com>
2024-08-01 17:51:59 +0900

    :package: 조자연: 필드조회 수정

--- db/read/read/Fieldread.ts
+++ db/read/read/Fieldread.ts
@@ -2,21 +2,24 @@
  * * 필드 조회
  * * @param selectname : 필드 이름 
  */
-import member from "../schema/Schema";
-import { IMember } from "../schema/Schemainterface";
+import mongoose from "mongoose";
+import productSchema from "../../products/schema/product.schema";
+import IProduct from "../../products/product.interface";
 
+const ProductModel = mongoose.model<IProduct>("Product", productSchema);
 
-const fieldread = async (selectname : string):Promise<IMember[]> => {//원하는 필드
+const fieldread = async (selectName: string): Promise<IProduct[]> => {
   try {
-    const query = { [selectname] : { $exists: true } };
-    const projection = { [selectname]: 1, _id: 0 }; 
-    const user = await member.find(query,projection).exec();
-    console.log("성공");
-    return user;
+    const query = { [selectName]: { $exists: true } };
+    const projection = { [selectName]: 1, _id: 0 };
+
+    const products = await ProductModel.find(query).select(projection).exec();
+    console.log("성공적으로 데이터를 조회하였습니다.");
+    return products;
   } catch (err) {
-    console.log(err);
-    throw err
+    console.error("데이터 조회 중 오류가 발생했습니다:", err);
+    throw err;
   }
 };
 
-export default fieldread
+export default fieldread;
