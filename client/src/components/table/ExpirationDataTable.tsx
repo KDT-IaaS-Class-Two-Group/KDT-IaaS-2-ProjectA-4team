323c8f7f69d9c97f39744b573749b20c1b9ad6fa
jojayeon <jojayeon6152@gmail.com>
2024-08-01 14:38:49 +0900

    :bulb: 조자연: 설명 추가

--- client/src/components/table/ExpirationDataTable.tsx
+++ client/src/components/table/ExpirationDataTable.tsx
@@ -6,27 +6,25 @@ import {
   TableHeader,
   TableRow,
   TableCell,
-} from "@../../components/ui/table";  // 실제 경로로 수정 필요
-import ButtonComponent from "../CustomButton"; // 실제 경로로 수정 필요
-import { ExpirationDateHook } from 'src/hooks/ExpirationDateHook'; // 수정된 훅 경로
+} from "@../../components/ui/table"; 
+import ButtonComponent from "../CustomButton"; 
+import { ExpirationDateHook } from 'src/hooks/ExpirationDateHook'; 
 
 export const ExpirationDataTable: React.FC = () => {
-  const { data, loading, error, deleteProduct } = ExpirationDateHook(); // 구조 분해 할당 
-
+  const { data, loading, error, deleteProduct } = ExpirationDateHook();  //구조 분해 할당 
   if (loading) {
     return <div>Loading...</div>;
   }
-
   if (error) {
     return <div>{error}</div>;
   }
 
   const sortedProducts = data.sort((a, b) => {
-    return new Date(a.expirationDate).getTime() - new Date(b.expirationDate).getTime();
+    return new Date(a.expirationDate).getTime() - new Date(b.expirationDate).getTime();  //오름차순 으로 정렬한 것
   });
 
   const handleDelete = (productID: number) => {
-    deleteProduct(productID.toString()); 
+    deleteProduct(productID.toString()); //이건 왜지 왜 숫자인 것을 스트링으로 변경하지 
   };
 
   return (
@@ -43,10 +41,11 @@ export const ExpirationDataTable: React.FC = () => {
         {sortedProducts.map((product) => (
           <TableRow key={product.productID}>
             <TableCell>{product.productName}</TableCell>
+            {/* <TableCell>{product.productingredient}</TableCell> //혹시 몰라서 요리들 재료 들어오는 거 생각해서 만들어준 것 이름은 변경해야함  */}
             <TableCell>{product.quantity}</TableCell>
             <TableCell>{new Date(product.expirationDate).toDateString()}</TableCell>
             <TableCell className="text-right">
-              <ButtonComponent variant="default" type="button" onClick={() => handleDelete(product.productID)}>폐기하기</ButtonComponent>
+              <ButtonComponent variant="default" type="button" onClick={() => handleDelete(product.productID)}>폐기하기</ButtonComponent>{/* handleDelete 데이터 가져오기*/}
             </TableCell>
           </TableRow>
         ))}
