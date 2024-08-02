0a3277a86f3b78f3cbcf92af847b86ba8a567669
jojayeon <jojayeon6152@gmail.com>
2024-08-02 12:54:44 +0900

    :package: 조자연: 로고 추가

--- client/src/pages/admin/adminstockDate.tsx
+++ client/src/pages/admin/adminstockDate.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import NavListBox from "src/components/admin/navListBox";
 import { ExpirationDataTable } from "src/components/table/ExpirationDataTable";
-
+import Logo from "src/components/logo";
 
 //사이드 바랑 메인 테이블 부분만 존재
 //로그 빠져있음
@@ -13,6 +13,11 @@ const AdminstockDate: React.FC = () => {
       <main>
         <div className="grid w-screen h-screen gap-4 p-3 grid-cols-custom-30-70">
           <NavListBox/>
+        </div>
+        <div>
+        <Logo className="border-2"
+          width={150}
+        />
         </div>
         <section>
           <div className="flex items-center justify-start w-50 h-auto p-3 border border-black border-solid">
