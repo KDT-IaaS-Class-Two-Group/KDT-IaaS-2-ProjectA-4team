04272fc10e672e454731c5026885ef139e4e4a58
moonhr <moonhr153@gmail.com>
2024-08-02 15:47:13 +0900

    :sparkles:문혜림: 사용자 로그인 접근시 토큰생성과 쿠키설정되는 메서드

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import IMember from '@db/members/member.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectModel('Member') private readonly memberModel: Model<IMember>,
+    private readonly jwtService: JwtService,
   ) {}
 
   async createUser(createUserDto: {
@@ -24,5 +26,28 @@ export class AuthService {
     });
 
     return await newMember.save();
+  }
+
+  /**
+   * @moonhr 24.08.02
+   * @param email
+   * @param roleId
+   * @returns jwt토큰, 쿠키설정
+   */
+  async generateToken(
+    email: string,
+    roleId: number,
+  ): Promise<{ token: string; cookieOptions: any }> {
+    console.log('토큰생서하러옴');
+    const payload = { email, roleId };
+    const token = this.jwtService.sign(payload);
+
+    const cookieOptions = {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production' || false,
+      maxAge: 3600000,
+    };
+    console.log('토큰 출력 직전');
+    return { token, cookieOptions };
   }
 }
