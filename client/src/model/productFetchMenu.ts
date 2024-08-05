2fe2c4ce32a56a2dca33e724c91a14957a1a1c6b
최유진 <yjyj0774@gmail.com>
2024-08-05 15:31:40 +0900

    :pencil2: 최유진 : 오타 수정

--- client/src/model/productFetchMenu.ts
+++ client/src/model/productFetchMenu.ts
@@ -1,5 +1,5 @@
 export default async () => {
-  const response = await fetch("http://localhose:3001/getProduct");
+  const response = await fetch("http://localhost:3001/getProduct");
 
   if (!response.ok) {
     throw new Error("Failed to fetch data");
