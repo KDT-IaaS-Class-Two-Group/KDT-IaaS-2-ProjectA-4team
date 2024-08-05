6971b8e7e8d8df81f0d85dfd3b176d5dbbf5b1be
jojayeon <jojayeon6152@gmail.com>
2024-08-05 16:26:57 +0900

    :package: 조자연 :엔드포이트 수정

--- client/src/hooks/ExpirationDateHook.tsx
+++ client/src/hooks/ExpirationDateHook.tsx
@@ -1,37 +1,47 @@
 import { useState, useEffect } from 'react';
 import { ProductDTO } from "../../../shared/DTO/products/product.dto";
 
+/**
+ * @jojayeon 24.08.05
+ * @returns {{
+*  data : ProductDTO[] 제품정보 , 
+*  loading : boolean 로딩 ,
+*  error : string | null 에러 ,
+*  deleteProduct: (id: string) => void 제품 정보 삭제
+* }}
+*/
+
 export const ExpirationDateHook = () => {
 const [data, setData] = useState<ProductDTO[]>([]);
 const [loading, setLoading] = useState<boolean>(true);
 const [error, setError] = useState<string | null>(null);
 
-const fetchData = async () => { // 데이터 가져온 것 훅으로 실행 시킬 코드역할과 업데이트할때 다시 실행될 코드
+const fetchData = async () => { 
   setLoading(true);
   try {
-    const response = await fetch('http://localhost:3001/products'); //endpoint - products
+    const response = await fetch('http://localhost:3001/productsDate'); //endpoint - productsDate
     if (!response.ok) {
       throw new Error('네트워크 응답이 올바르지 않습니다.');
     }
     const result = await response.json();
-    setData(result); // 가져온 데이터 
+    setData(result);
   } catch (err) {
-    setError('데이터를 가져오는 데 실패했습니다.'); // 에러
+    setError('데이터를 가져오는 데 실패했습니다.'); 
   } finally {
-    setLoading(false);//로딩 메시지 제거
+    setLoading(false);
   }
 };
 
-useEffect(() => { //마운트 될 때 사용된다. 
+useEffect(() => { 
   fetchData();
 }, []);
 
 const deleteProduct = async (_id: string) => {
   try {
-    await fetch(`http://localhost:3001/products/${_id}`, {     //엔드 포인트 products/${productID}` - 받는 부분은 어떻게 설정하지 변경해야하나?
-      method: 'DELETE'                                               // 이거 때문에 문제가 없는 것 처럼 이야기 하는데 맞는지 모르겠음
+    await fetch(`http://localhost:3001/productsDate/${_id}`, {
+      method: 'DELETE'                                               
     });
-    fetchData(); // 삭제 후 데이터 갱신 훅 
+    fetchData();
   } catch (err) {
     setError('데이터를 삭제하는 데 실패했습니다.');
   }
