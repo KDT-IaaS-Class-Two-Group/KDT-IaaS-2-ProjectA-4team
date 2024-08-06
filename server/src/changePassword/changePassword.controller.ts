1d0c3b7283e02d1cd2aedc07323bf4bbda938c61
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-06 09:55:28 +0900

    :package:유호영:토큰 사용으로 인해 백엔드 측 비밀번호 변경 컨트롤러, 서비스 변경

--- server/src/changePassword/changePassword.controller.ts
+++ server/src/changePassword/changePassword.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Req, Res, HttpStatus } from '@nestjs/common';
 import { PasswordService } from './changePassword.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { Request,Response } from 'express';
 @UseGuards(JwtAuthGuard)
 @Controller()
 export class PasswordController {
@@ -8,10 +9,25 @@ export class PasswordController {
 
   @Post('changePassword')
   async changePassword(
-    @Body('name') name: string,
+    @Req() req:Request,
     @Body('password') oldPassword: string,
     @Body('changePassword') newPassword: string,
+    @Res() res:Response
   ): Promise<any> {
-    return this.passwordService.changePassword(name, oldPassword, newPassword);
+    const token = req.cookies['token'];
+    if(!token){
+      res.status(HttpStatus.UNAUTHORIZED).json({message: "인증되지 않았습니다."});
+      return;
+    }
+    try {
+      const decoded = this.passwordService.verifyToken(token);
+      const userEmail = decoded.email;
+      await this.passwordService.changePassword(userEmail, oldPassword, newPassword);
+      res.status(HttpStatus.OK).json({message: "비밀번호가 성공적으로 변경되었습니다."});
+    } catch (error) {
+      if(error instanceof Error){
+        res.status(HttpStatus.BAD_REQUEST).json({ message: error.message });
+      }
+    }
   }
 }
--- server/src/changePassword/changePassword.service.ts
+++ server/src/changePassword/changePassword.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import IMember from '@db/members/member.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 /**
@@ -12,14 +13,19 @@ import IMember from '@db/members/member.interface';
 export class PasswordService {
   constructor(
     @InjectModel('Member') private readonly memberModel: Model<IMember>,
+    private readonly jwtService: JwtService,
   ) {}
 
+  verifyToken(token:string) {
+    return this.jwtService.verify(token);
+  }
+
   async changePassword(
-    name: string,
+    email: string,
     oldPassword: string,
     newPassword: string,
   ): Promise<any> {
-    const member = await this.memberModel.findOne({ name });
+    const member = await this.memberModel.findOne({ email });
     if (!member) {
       throw new NotFoundException('사용자를 찾을 수 없습니다.');
     }
