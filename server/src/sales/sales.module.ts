cf7cd5f95e4d5313643e730dc281c8f11da6223b
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-06 15:37:34 +0900

    :heavy_plus_sign:유호영:매출관련 모듈에 토큰 사용을 위한 auth module로드

--- server/src/sales/sales.module.ts
+++ server/src/sales/sales.module.ts
@@ -3,9 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import saleSchema from '@db/sale/schema/sale.schema';
 import { SaleService } from './sales.service';
 import { SaleController } from './sales.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Sale', schema: saleSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: 'Sale', schema: saleSchema }]),
+    AuthModule
+  ],
   providers: [SaleService],
   controllers: [SaleController],
 })
