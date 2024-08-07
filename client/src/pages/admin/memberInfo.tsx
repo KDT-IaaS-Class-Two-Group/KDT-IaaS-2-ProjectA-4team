5ba781dee181c3d44b91f7a87400b49339691910
이종수 <whdtnxd@gmail.com>
2024-08-07 15:42:23 +0900

    :recycle: 이종수: 상품 데이터 테이블 fetching 환경 변수 && 모듈 이용한 방식으로 변경

--- client/src/pages/admin/memberInfo.tsx
+++ client/src/pages/admin/memberInfo.tsx
@@ -4,13 +4,17 @@ import { GetServerSideProps } from "next";
 import AdminNav from "src/components/admin/adminNav";
 import MemberInfoTable from "src/components/table/MemberInfoTable";
 import IMemberInfo from "../../interfaces/MemberInfo.interface";
+import url3001Generator from "src/modules/generator/url3001Generator";
 
 interface Props {
   members: IMemberInfo[];
 }
 
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
-  const res = await fetch("http://localhost:3001/api/members");
+  const EP_API = process.env.NEXT_PUBLIC_EP_API as string;
+  const EP_MEMBERS = process.env.NEXT_PUBLIC_EP_MEMBERS as string;
+
+  const res = await fetch(url3001Generator(EP_API, EP_MEMBERS));
 
   if (!res.ok) {
     return {
