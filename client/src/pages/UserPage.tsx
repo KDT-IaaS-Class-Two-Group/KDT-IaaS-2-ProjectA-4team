3617621a535a5aa8d37d3cbb15f519aa083a5db5
최유진 <yjyj0774@gmail.com>
2024-08-07 17:33:03 +0900

    :art: 최유진 : 구매하기버튼 스타일 요소 추가

--- client/src/pages/UserPage.tsx
+++ client/src/pages/UserPage.tsx
@@ -59,7 +59,7 @@ const UserPage: FC = () => {
         <Cart items={cartItems} removedItem={handleRemoveItem} />
         <ButtonComponent
           type="submit"
-          className="w-60 bg-yellow-400 text-white hover:text-yellow-400 hover:border-yellow-400 hover:border text-lg font-bold rounded-3xl mx-6 my-8"
+          className="w-60 bg-yellow-400 text-white hover:bg-white hover:text-yellow-400 hover:border-yellow-400 hover:border text-lg font-bold rounded-3xl mx-6 my-8"
           onClick={() => console.log("buy!")}
         >
           Buy !
