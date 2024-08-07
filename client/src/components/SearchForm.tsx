df9e176b5dcc1e53131775f02135120c80854893
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-07 14:57:20 +0900

    :package:유호영:주문내역 연월일까지만 표시

--- client/src/components/SearchForm.tsx
+++ client/src/components/SearchForm.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import InputComponent from './Input';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import InputComponent from "./Input";
 
 const SearchForm: React.FC = () => {
   return (
