e2435ff79cd6bd587852ed341fe76b2c0131b3d5
최유진 <yjyj0774@gmail.com>
2024-08-08 12:15:38 +0900

    :white_check_mark: 최유진 : 에러 처리

--- client/src/model/productFetchMenu.ts
+++ client/src/model/productFetchMenu.ts
@@ -1,11 +1,15 @@
 export default async () => {
-  const response = await fetch("http://localhost:3001/product");
+  try {
+    const response = await fetch("http://localhost:3001/product");
 
-  if (!response.ok) {
-    throw new Error("Failed to fetch data");
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log(error);
   }
-
-  const data = await response.json();
-
-  return data;
 };
