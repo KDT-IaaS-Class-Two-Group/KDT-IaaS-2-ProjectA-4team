110ad01d641e32786bb10b66ba7926dd9bbf653d
jojayeon <jojayeon6152@gmail.com>
2024-08-08 21:33:01 +0900

    :package: 조자연: 추가 버튼 페이지 추가

--- client/src/pages/admin/stockDate.tsx
+++ client/src/pages/admin/stockDate.tsx
@@ -3,6 +3,7 @@ import AdminNav from "src/components/admin/adminNav";
 import { ExpirationDataTable } from "src/components/table/ExpirationData/ExpirationDataTable";
 import Logo from "src/components/logo";
 import TitleComponent from "src/components/titleComponent";
+import ProductPage from "src/components/table/ExpirationData/savedatabutton";
 /**
  * @jojayeon 24.08.05
  * @returns 유통관리 페이지
@@ -19,6 +20,7 @@ const AdminstockDate: React.FC = () => {
           <div className="flex items-center justify-start w-50 h-auto p-3 border border-black border-solid">
             <Logo width={100} height={100} alt="Custom Logo" className="pr-5" />
             <TitleComponent titletext="유통기한 관리" />
+            <ProductPage/>
           </div>
           <ExpirationDataTable />
         </div>
