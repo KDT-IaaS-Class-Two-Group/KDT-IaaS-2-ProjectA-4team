b29cf6aa3c93ba5144ad5bf119de5a260d0820bd
moonhr <moonhr153@gmail.com>
2024-08-08 14:33:26 +0900

    :package:문혜림: nest 쿠키파서 설정 부활

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.use(cookieParser());
+  app.use(cookieParser());
   app.enableCors({
     origin: 'http://localhost:3000',
     methods: 'GET, POST, PUT, PATCH, DELETE',
