eb362171c65cdce83ddbb8bc1afa0a7e2d536f87
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-08 12:40:41 +0900

    :bug:유호영:마이페이지 수정 중

--- client/src/hooks/footer/info/useFooterInfoHook.ts
+++ client/src/hooks/footer/info/useFooterInfoHook.ts
@@ -11,15 +11,18 @@ const useFooterInfoHook = () => {
 
   useEffect(() => {
     const fetchUserInfo = async () => {
-      const EP_USER_INFO = process.env.NEXT_PUBLIC_U_INFO as string;
+      const EP_USER_INFO = process.env.NEXT_PUBLIC_EP_U_INFO as string;
+      console.log(url3001Generator(EP_USER_INFO));
       try {
         const response = await fetch(url3001Generator(EP_USER_INFO), {
           method: "GET",
           credentials: "include",
         });
+        console.log(response);
         const result = await response.json();
         if (response.ok) {
           setUserName(result.email); // 이메일로 사용자 이름 설정
+          console.log(result);
         } else {
           console.error("Error fetching user info:", result.message); // 에러 메시지 로그
           setUserName(null);
