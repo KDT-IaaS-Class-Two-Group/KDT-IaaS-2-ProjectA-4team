29c437db7ba9bb439ec1b672330dd46dea6d774e
이종수 <whdtnxd@gmail.com>
2024-08-09 17:35:20 +0900

    :recycle: 이종수: userPageHook.ts 파일 내 정의된 인터페이스 새 파일로 분리 && 정적 자원 static 폴더로 이관

---
+++ client/static/hooks/user/userPageHook.static.ts
@@ -0,0 +1,7 @@
+export const userPageHookErrMessages = {
+  failedGetCategories: "카테고리를 불러오지 못했습니다.",
+
+  failedAddCategories: "장바구니에 아이템을 추가하지 못했습니다.",
+
+  failedDeleteCategories: "장바구니의 아이템을 삭제하지 못했습니다.",
+};
