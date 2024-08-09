c2583694237d9ad10e04d595224481af4a7c7b43
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-09 11:10:43 +0900

    :recycle:유호영:주문내역 변경

--- client/src/types/order/Order.type.ts
+++ client/src/types/order/Order.type.ts
@@ -3,8 +3,10 @@ interface TOrder {
   products: {
     productName: string;
     unitPrice: number;
-  };
+    quantity: number;
+  }[];
   saleDate: string;
+  totalPrice: number; // 총 가격 추가
 }
 
 export default TOrder;
