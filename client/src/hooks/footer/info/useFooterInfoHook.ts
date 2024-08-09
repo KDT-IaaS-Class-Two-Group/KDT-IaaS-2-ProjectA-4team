d3e704b692cf74c1cde797f1fd3fd09ffa0b121d
이종수 <whdtnxd@gmail.com>
2024-08-09 14:03:48 +0900

    :recycle: 이종수: userFotterInfoHook 모듈 이용한 네트워크 통신 방식으로 변경

--- client/src/hooks/footer/info/useFooterInfoHook.ts
+++ client/src/hooks/footer/info/useFooterInfoHook.ts
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
+import fetcher from "src/modules/fetching/fetcher";
 import serverUrlGenerator from "src/modules/generator/serverUrlGenerator";
+import thrower from "src/modules/throw/thrower";
+import { failFetchedUserInfoMessage } from "static/error/hooks/footer/info/useFooterInfoHook";
 
 interface CustomJwtPayload {
   name: string;
@@ -13,15 +16,16 @@ const useFooterInfoHook = () => {
     const fetchUserInfo = async () => {
       const EP_USER_INFO = process.env.NEXT_PUBLIC_EP_U_INFO as string;
       try {
-        const response = await fetch(serverUrlGenerator(EP_USER_INFO), {
-          method: "GET",
-          credentials: "include",
-        });
+        const response = await fetcher(
+          serverUrlGenerator(EP_USER_INFO),
+          "get",
+          { credentials: "include" },
+        );
         const result = await response.json();
         if (response.ok) {
           setUserName(result.email); // 이메일로 사용자 이름 설정
         } else {
-          console.error("Error fetching user info:", result.message); // 에러 메시지 로그
+          thrower(`${failFetchedUserInfoMessage}: ${result.message}`); // 에러 메시지 로그
           setUserName(null);
         }
       } catch (error) {
