78a5b6f0779cf2bc72efb2d741a38de6021fb42d
moonhr <moonhr153@gmail.com>
2024-08-09 13:12:46 +0900

    :sparkles:문혜림: 로그아웃 시 서버로 요청

--- client/src/components/footer/footerComponent.tsx
+++ client/src/components/footer/footerComponent.tsx
@@ -2,9 +2,34 @@
 import React from "react";
 import { useRouter } from "next/router";
 import useFooterInfoHook from "src/hooks/footer/info/useFooterInfoHook";
+import url3001Generator from "src/modules/generator/url3001Generator";
 
 interface FooterLinksProps {
   className?: string;
+}
+
+async function logout(): Promise<void> {
+  try {
+    const LOGOUT = process.env.NEXT_PUBLIC_LOGOUT as string;
+    // 서버에 로그아웃 요청 보내기
+    const response = await fetch(url3001Generator(LOGOUT), {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+
+    if (!response.ok) {
+      // 서버에서 오류가 발생한 경우
+      throw new Error(`Logout failed with status ${response.status}`);
+    }
+    // 로그아웃 후 리다이렉트
+    window.location.href = "/";
+  } catch (error) {
+    console.error("Error during logout:", error);
+    // 로그아웃 오류에 대한 처리, 예를 들어 사용자에게 알림 표시
+  }
 }
 
 /**
@@ -32,10 +57,7 @@ const FooterLinks: React.FC<FooterLinksProps> = ({ className }) => {
       >
         마이페이지
       </p>
-      <p
-        onClick={() => handleNavigation("/logout")}
-        className="cursor-pointer hover:text-black"
-      >
+      <p onClick={logout} className="cursor-pointer hover:text-black">
         로그아웃
       </p>
     </div>
