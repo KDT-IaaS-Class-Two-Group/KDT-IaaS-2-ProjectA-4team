8911bb5136df76a79df6c207399159444c71d48b
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-09 11:10:24 +0900

    :heavy_plus_sign:유호영:sales module에서 members모델을 참조

--- server/src/sales/sales.controller.ts
+++ server/src/sales/sales.controller.ts
@@ -18,7 +18,7 @@ export class SaleController {
   }
 
   @Get('orders/:name')
-  async findByMemberID(@Param('name') name: string): Promise<ISale[]> {
-    return this.saleService.findByMemberID(name);
+  async findByMemberName(@Param('name') name: string): Promise<ISale[]> {
+    return this.saleService.findByMemberName(name);
   }
 }
