0515d1024cb7ee52142a9d9e1f85d2ea4188cd4f
jojayeon <jojayeon6152@gmail.com>
2024-08-09 15:32:54 +0900

    조자연 :파알 이동

---
+++ client/src/components/modal/expiration/ExpirationDateModal.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from "@radix-ui/react-dialog";
+import ButtonComponent from "../../button/customized/CustomButton";
+import ConfirmDeleteModalProps from "src/interfaces/validation/expiration/ExpirationDateinterface";
+/**
+ * @jojayeon 24.08.07
+ * * 유통관리 페이지 폐기버튼 모달창
+ * @param {boolean} props.open - 모달이 열려 있는지의 여부
+ * @param {() => void} props.onClose - 모달을 닫는 함수
+ * @param {() => void} props.onConfirm - 삭제 확인을 처리하는 함수
+ * @returns JSX.Element - 폐기 모달창
+ */
+export const ConfirmDeleteModal: React.FC<ConfirmDeleteModalProps> = ({
+  open,
+  onClose,
+  onConfirm,
+  title,
+  content,
+}) => {
+  return (
+    <Dialog open={open} onOpenChange={onClose}>
+      {open && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 z-40"
+          onClick={onClose}
+          aria-hidden="true"
+        />
+      )}
+      <DialogContent className="fixed inset-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg p-6 shadow-lg z-50 w-full max-w-md h-1/5">
+        <DialogTitle className="text-xl font-bold mb-4">{title}</DialogTitle>
+        <DialogTrigger className="mb-4">{content}</DialogTrigger>
+        <div className="flex justify-end gap-4">
+          <ButtonComponent onClick={onClose} variant="outline">
+            취소
+          </ButtonComponent>
+          <ButtonComponent onClick={onConfirm} variant="default">
+            확인
+          </ButtonComponent>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
---
+++ client/src/components/table/expiration/ExpirationDataTable.tsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableCell,
+} from "@../../components/ui/table";
+import ButtonComponent from "../../button/customized/CustomButton";
+import { ExpirationDateHook } from "src/hooks/expiration/ExpirationDateHook";
+import { ConfirmDeleteModal } from "../../modal/expiration/ExpirationDateModal";
+
+/**
+ * @jojayeon 24.08.07
+ * @returns 유통기한 관리 테이블
+ */
+
+export const ExpirationDataTable: React.FC = () => {
+  const { data, loading, error, deleteProduct } = ExpirationDateHook();
+  const [open, setOpen] = useState(false);
+  const [selectedProductId, setSelectedProductId] = useState<string | null>(
+    null,
+  );
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>{error}</div>;
+  }
+  //정렬하기 날짜별, 이름별 2번
+  const sortedProducts = data.sort((a, b) => {
+    const expirationDateComparison =
+      new Date(a.expirationDate).getTime() -
+      new Date(b.expirationDate).getTime();
+
+    if (expirationDateComparison !== 0) {
+      return expirationDateComparison;
+    }
+
+    return a.productName.localeCompare(b.productName);
+  });
+
+  // 도달창 띄우고 취소 확인
+  //확인
+  const handleDelete = () => {
+    if (selectedProductId) {
+      deleteProduct(selectedProductId);
+      setOpen(false);
+    }
+  };
+
+  //폐기 버튼
+  const openModal = (product: string) => {
+    setSelectedProductId(product);
+    setOpen(true);
+  };
+
+  //취소 버튼
+  const closeModal = () => {
+    setOpen(false);
+    setSelectedProductId(null);
+  };
+
+  return (
+    <>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>분류</TableHead>
+            <TableHead>제품명</TableHead>
+            <TableHead>수량</TableHead>
+            <TableHead className="">유통기한</TableHead>
+            <TableHead className="text-right pr-10">폐기</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {sortedProducts.map((product) => (
+            <TableRow key={product._id}>
+              <TableCell>{product.productCategory}</TableCell>
+              <TableCell>{product.productName}</TableCell>
+              <TableCell>{product.quantity}</TableCell>
+              <TableCell>
+                {new Date(product.expirationDate).toDateString()}
+              </TableCell>
+              <TableCell className="text-right">
+                <ButtonComponent
+                  variant="default"
+                  type="button"
+                  onClick={() => openModal(product._id)}
+                >
+                  폐기하기
+                </ButtonComponent>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      {/* 모달창 */}
+      <ConfirmDeleteModal
+        open={open}
+        onClose={closeModal}
+        onConfirm={handleDelete}
+        title=""
+        content=""
+      />
+    </>
+  );
+};
