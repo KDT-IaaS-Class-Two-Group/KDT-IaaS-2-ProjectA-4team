689a8d4a0a69d08c7910592f2ec4ef0ba95db3bb
최유진 <yjyj0774@gmail.com>
2024-08-09 11:58:11 +0900

    :sparkles: 최유진 : getUserEmail 엔드포인트에서 사용자 이메일 가져오기

---
+++ client/src/model/user/email/getUserEmailFetch.ts
@@ -0,0 +1,16 @@
+export default async (): Promise<string> => {
+  try {
+    const response = await fetch("http://localhost:3001/getUserEamil", {
+      method: "GET",
+      credentials: "include",
+    });
+    if (!response.ok) {
+      throw new Error("사용자 이메일을 가져오는 데 실패했습니다.");
+    }
+    const data = await response.json();
+    return data.userEmail;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
