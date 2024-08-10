02ac74f9a49b9483e3851a9339b5264301d128e6
moonhr <moonhr153@gmail.com>
2024-08-10 15:14:47 +0900

    :sparkles:문혜림: 토큰을 검증하여 사용자 고유id, name, roleid를 반환하는 유틸 제작

---
+++ server/src/utils/token.utils.ts
@@ -0,0 +1,69 @@
+import { JwtService } from '@nestjs/jwt';
+import { Types } from 'mongoose';
+
+interface DecodedToken {
+  email: string;
+  name: string;
+  roleID: string;
+}
+
+/**
+ * @moonhr 24.08.10
+ * * 토큰을 검증, 디코딩하여 사용자정보를 반환.
+ */
+export class TokenUtils {
+  constructor(private jwtService: JwtService) {}
+
+  // 토큰을 검증하고 디코딩하여 사용자 정보를 반환하는 함수
+  verifyAndDecodeToken(token: string): DecodedToken | null {
+    try {
+      const decoded = this.jwtService.verify<DecodedToken>(token);
+      return decoded;
+    } catch (error) {
+      console.error('Error in verifyAndDecodeToken:', error);
+      return null;
+    }
+  }
+
+  // 토큰에서 사용자 ID를 추출하는 함수
+  async findMemberIdByToken(
+    token: string,
+    memberModel: any,
+  ): Promise<Types.ObjectId | null> {
+    const decoded = this.verifyAndDecodeToken(token);
+    if (!decoded) {
+      throw new Error('Invalid token');
+    }
+
+    const user = await memberModel.findOne({ email: decoded.email }).exec();
+    return user ? user._id : null;
+  }
+
+  // 토큰에서 사용자 name을 추출하는 함수
+  async findNameByToken(
+    token: string,
+    memberModel: any,
+  ): Promise<string | null> {
+    const decoded = this.verifyAndDecodeToken(token);
+    if (!decoded) {
+      throw new Error('Invalid token');
+    }
+
+    const user = await memberModel.findOne({ email: decoded.email }).exec();
+    return user ? user.name : null;
+  }
+
+  // 토큰에서 사용자 권한을 추출하는 함수
+  async findRoleIdByToken(
+    token: string,
+    memberModel: any,
+  ): Promise<string | null> {
+    const decoded = this.verifyAndDecodeToken(token);
+    if (!decoded) {
+      throw new Error('Invalid token');
+    }
+
+    const user = await memberModel.findOne({ email: decoded.email }).exec();
+    return user ? user.roleID : null;
+  }
+}
