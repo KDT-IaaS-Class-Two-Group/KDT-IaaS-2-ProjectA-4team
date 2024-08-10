72935a4e805926ab644fc61a85a7594acb6b7776
moonhr <moonhr153@gmail.com>
2024-08-10 17:35:44 +0900

    :package:문혜림: 로그인 로그 절차 생성
    로그인 로그 요청이 들어오면
    토큰을 검증하고
    id를 추출하여 로그를 생성한다.

--- server/src/log/logs.controller.ts
+++ server/src/log/logs.controller.ts
@@ -1,31 +1,34 @@
-import { Controller, Post } from "@nestjs/common";
-import { UserLogService } from "./userlog.service";
+import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { UserLogService } from './userlog.service';
+import { Request } from 'express';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('log')
-export class LogsController{
-  constructor(private readonly userLogService:UserLogService){}
+export class LogsController {
+  constructor(private readonly userLogService: UserLogService) {}
 
   //로그인 로그
-  @Post('login'){
-
+  @Post('login')
+  async login(@Req() req: Request) {
+    const token = req.cookies['token'];
+    //토큰검사하여 id 추출
+    //로그생성
   }
+
   //로그아웃 로그
-  @Post('logout'){
-
-  }
+  @Post('logout')
+  async logout() {}
   //사용자 구매 로그
-  @Post('purchase'){
-
-  }
+  @Post('purchase')
+  async purchase() {}
   //재고 추가 로그
-  @Post('addStock'){
-
-  }
+  @Post('addStock')
+  async addStock() {}
   //재고 폐기 로그
-  @Post('delStock'){
-    
-  }
+  @Post('delStock')
+  async delStock() {}
   //메뉴 추가 로그
   @Post('addMenu')
+  async addMenu() {}
 }
-
