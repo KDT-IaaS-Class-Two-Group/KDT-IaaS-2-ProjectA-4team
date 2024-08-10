9075955184acebe125649c4f34bcc9e9f9dc5e1c
moonhr <moonhr153@gmail.com>
2024-08-10 16:56:49 +0900

    :package:문혜림:로그 모듈이 로그 컨트롤러를 사용할수 있도록 수정

--- server/src/log/logs.module.ts
+++ server/src/log/logs.module.ts
@@ -2,10 +2,12 @@ import { UserLogService } from './userlog.service';
 import { UserLog, UserLogSchema } from '../schemas/userLog.schema';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { LogsController } from './logs.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: UserLog.name, schema: UserLogSchema }]),
+    LogsController,
   ],
   providers: [UserLogService],
   exports: [UserLogService],
