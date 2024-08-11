2a3f204f4de7290e69037f3fc5506d4304e47a7c
moonhr <moonhr153@gmail.com>
2024-08-11 22:01:35 +0900

    :memo:문혜림:주석 추가/삭제

--- server/src/log/logs.controller.ts
+++ server/src/log/logs.controller.ts
@@ -49,6 +49,7 @@ export class LogsController {
       throw new Error('Failed to create logout log');
     }
   }
+  //TODO 구매로직 완성되면 추가
   //사용자 구매 로그
   @Post('purchase')
   async purchase(@Req() req: Request, @Body() data) {
@@ -113,6 +114,7 @@ export class LogsController {
       throw new Error('Failed to create delStock log');
     }
   }
+  //TODO 매뉴추가로직 완성되면 추가
   //메뉴 추가 로그
   @Post('addMenu')
   async addMenu(@Req() req: Request, @Body() data) {
