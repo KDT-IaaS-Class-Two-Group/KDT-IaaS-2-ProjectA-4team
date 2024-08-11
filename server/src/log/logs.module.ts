776ab7be714c7fd25f68330b36f32b612faca4d8
moonhr <moonhr153@gmail.com>
2024-08-11 20:27:30 +0900

    :package:문혜림: 로그 컨트롤러가 jwt모듈을 사용할 수있도록 수정

--- server/src/log/logs.module.ts
+++ server/src/log/logs.module.ts
@@ -3,13 +3,28 @@ import { UserLog, UserLogSchema } from '../schemas/userLog.schema';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { LogsController } from './logs.controller';
+import { TokenUtils } from '../utils/token.utils';
+import { Member, MemberSchema } from '../schemas/member.schema';
+import { JwtStrategy } from '../auth/jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: UserLog.name, schema: UserLogSchema }]),
-    LogsController,
+    MongooseModule.forFeature([{ name: Member.name, schema: MemberSchema }]),
+    ConfigModule.forRoot(),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'), // 환경 변수에서 비밀 키 가져옴
+        signOptions: { expiresIn: '1h' },
+      }),
+    }),
   ],
-  providers: [UserLogService],
+  controllers: [LogsController],
+  providers: [UserLogService, TokenUtils, JwtStrategy],
   exports: [UserLogService],
 })
 export class LogsModule {}
