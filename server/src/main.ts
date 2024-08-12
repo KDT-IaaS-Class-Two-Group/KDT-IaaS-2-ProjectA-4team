101dd85603b9e32d7d4e3bbad4f81ef206e5404d
eonduck2 <whdtnxd@gmail.com>
2024-08-13 02:42:32 +0900

    :package: 이종수: mixed contents 에러 해결을 위한 패키지 적용

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,16 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+
+  app.use(helmet());
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", 'data:', 'https:'],
+        connectSrc: ["'self'", 'https://release.dyi1fscmksq0r.amplifyapp.com'],
+        upgradeInsecureRequests: [],
+      },
+    }),
+  );
+
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://release.dyi1fscmksq0r.amplifyapp.com',
     methods: 'GET, POST, PUT, PATCH, DELETE',
     allowedHeaders: 'Content-type, Authorization',
     credentials: true,
   });
+
   await app.listen(3001);
   console.log(`http://localhost:3001`);
 }
