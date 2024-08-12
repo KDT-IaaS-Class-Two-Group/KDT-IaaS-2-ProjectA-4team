c98eb13b036ba872564130c403985a1445e94ab1
최유진 <yjyj0774@gmail.com>
2024-08-12 23:24:30 +0900

    :art: 최유진 : 재고조회 테이블 컴포넌트 디자인 1차 수정

--- client/src/components/table/Product/ProductTable.tsx
+++ client/src/components/table/Product/ProductTable.tsx
@@ -83,7 +83,7 @@ const ProductTable: React.FC = () => {
               variant="default"
               type="button"
               onClick={() => openOrderModal(row)}
-              className="mb-1 text-xs bg-slate-500 hover:bg-black"
+              className="text-gray-500 underline text-xs leading-loose hover:text-black"
             >
               발주하기
             </ButtonComponent>
@@ -91,7 +91,7 @@ const ProductTable: React.FC = () => {
               variant="default"
               type="button"
               onClick={() => openUpdateModal(row)}
-              className="mt-1 text-xs bg-slate-500 hover:bg-black"
+              className="mt-1 text-gray-500 underline text-xs leading-loose hover:text-black"
             >
               수정하기
             </ButtonComponent>
