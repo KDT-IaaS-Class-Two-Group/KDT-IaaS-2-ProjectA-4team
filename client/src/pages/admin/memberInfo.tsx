719011e3b5034be57430517347c745e2bf05655b
최유진 <yjyj0774@gmail.com>
2024-08-12 23:26:35 +0900

    :art: 최유진 : 회원정보 테이블 컴포넌트 디자인 1차 수정

--- client/src/pages/admin/memberInfo.tsx
+++ client/src/pages/admin/memberInfo.tsx
@@ -4,6 +4,7 @@ import MemberInfoTable from "src/components/table/member/MemberInfoTable";
 import IMemberInfo from "../../interfaces/member/MemberInfo.interface";
 import serverUrlGenerator from "src/modules/generator/serverUrlGenerator";
 import fetcher from "src/modules/fetching/fetcher";
+import TitleComponent from "src/components/title/titleComponent";
 
 /**
  * @crystal23733 24.08.12
@@ -19,19 +20,23 @@ const MemberInfoPage: React.FC = () => {
       try {
         const EP_API = process.env.NEXT_PUBLIC_EP_API as string;
         const EP_MEMBERS = process.env.NEXT_PUBLIC_EP_MEMBERS as string;
-        
-        const res = await fetcher(serverUrlGenerator(EP_API, EP_MEMBERS), "get", {
-          credentials: "include"
-        });
+
+        const res = await fetcher(
+          serverUrlGenerator(EP_API, EP_MEMBERS),
+          "get",
+          {
+            credentials: "include",
+          },
+        );
 
         if (!res.ok) {
-          throw new Error('Failed to fetch members');
+          throw new Error("Failed to fetch members");
         }
 
         const data: IMemberInfo[] = await res.json();
         setMembers(data);
       } catch (err) {
-        setError('Failed to load data');
+        setError("Failed to load data");
         console.error(err);
       } finally {
         setLoading(false);
@@ -52,9 +57,14 @@ const MemberInfoPage: React.FC = () => {
   }));
 
   return (
-    <div className="flex w-svw h-svh">
-      <AdminNav />
-      <div className="flex flex-col w-full">
+    <div className="grid w-screen gap-2 p-3 overflow-hidden grid-cols-custom-30-70">
+      <div>
+        <AdminNav />
+      </div>
+      <div className="flex flex-col gap-4 mr-5">
+        <div className="items-center w-auto h-10 py-3">
+          <TitleComponent titletext="회원 조회" />
+        </div>
         <MemberInfoTable
           head={["Data ID", "이름", "이메일", "관리자 권한"]}
           data={formattedData}
