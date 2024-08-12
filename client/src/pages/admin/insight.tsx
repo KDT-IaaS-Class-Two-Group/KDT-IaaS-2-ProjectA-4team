7681bf69b0cd88df5187823b829d09920bcde4c5
moonhr <moonhr153@gmail.com>
2024-08-12 16:48:35 +0900

    :sparkles:문혜림: 인사이트 페이지 제작

--- client/src/pages/admin/insight.tsx
+++ client/src/pages/admin/insight.tsx
@@ -1,1 +1,16 @@
 import React from "react";
+import AdminNav from "src/components/nav/admin/adminNav";
+// import InsigthData from "src/components/insigth/insigthData";
+import DatePickerWithRange from "src/components/insigth/insigthData";
+
+const InsightPage: React.FC = () => {
+  return (
+    <>
+      <AdminNav />
+      {/* <InsigthData /> */}
+      <DatePickerWithRange />
+    </>
+  );
+};
+
+export default InsightPage;
