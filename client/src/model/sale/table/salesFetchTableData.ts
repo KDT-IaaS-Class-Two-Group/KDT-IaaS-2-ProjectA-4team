0f451ad8d7acca7965072b6d4369e8964d0dd1d2
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-14 01:46:40 +0900

    :package:유호영:매출관리 페이지 의존성 변경

--- client/src/model/sale/table/salesFetchTableData.ts
+++ client/src/model/sale/table/salesFetchTableData.ts
@@ -1,19 +1,20 @@
 import fetcher from "src/modules/fetching/fetcher";
-import { SaleDTO } from "../../../../../shared/DTO/sale/sale.dto";
 import serverUrlGenerator from "src/modules/generator/serverUrlGenerator";
+import { ClientSaleDTO } from "@shared/DTO/sale/clientSale.interface";
 
 /**
  * @crystal23733 24.07.30
  * @return data 매출관련 데이터
  */
-export default async (): Promise<SaleDTO[]> => {
+export default async (): Promise<ClientSaleDTO[]> => {
   const EP_SALES = process.env.NEXT_PUBLIC_EP_SALES as string;
 
   const response = await fetcher(serverUrlGenerator(EP_SALES), "get", {
     credentials: "include",
   });
 
-  const data = await response.json();
+  const data: ClientSaleDTO[] = await response.json();
   console.log(data);
+
   return data;
 };
