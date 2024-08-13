952e26286c9c54becf18460e77377375b2001fef
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-13 15:28:32 +0900

    :package:유호영:change password변경

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -106,8 +106,10 @@ async changePassword(
   }
   try {
     const decoded = this.authService.verifyToken(token);
-    const userName = decoded.name;
-    await this.authService.changePassword(userName, oldPassword, newPassword);
+    console.log("decoded", decoded);
+    const userEmail = decoded.email;
+    console.log("change password:", userEmail);
+    await this.authService.changePassword(userEmail, oldPassword, newPassword);
     return res
       .status(HttpStatus.OK)
       .json({ message: '비밀번호가 성공적으로 변경되었습니다.' });
