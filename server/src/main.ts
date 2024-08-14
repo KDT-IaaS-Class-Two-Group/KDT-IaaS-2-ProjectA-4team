25595fa73f43bcc86000ef77384c21f1e042bb4b
이종수 <whdtnxd@gmail.com>
2024-08-14 12:29:45 +0900

    :package: 이종수: 배포 테스트

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,36 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
-import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-
-  app.use(helmet());
-  app.use(
-    helmet.contentSecurityPolicy({
-      directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
-        styleSrc: ["'self'", "'unsafe-inline'"],
-        imgSrc: ["'self'", 'data:', 'https:'],
-        connectSrc: ["'self'", 'https://release.dyi1fscmksq0r.amplifyapp.com'],
-        // upgradeInsecureRequests: [],
-      },
-    }),
-  );
-
   app.enableCors({
-    origin: [
-      'https://release.dyi1fscmksq0r.amplifyapp.com',
-      'http://13.125.186.170:3001',
-    ],
+    origin: 'http://localhost:3000',
     methods: 'GET, POST, PUT, PATCH, DELETE',
     allowedHeaders: 'Content-type, Authorization',
     credentials: true,
   });
-
   await app.listen(3001);
   console.log(`http://localhost:3001`);
 }
