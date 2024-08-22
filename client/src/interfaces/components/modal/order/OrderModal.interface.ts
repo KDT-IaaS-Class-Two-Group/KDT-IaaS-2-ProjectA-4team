b509ada87d5ae52adb6efe5606fb1f18600e5f94
moonhr <moonhr153@gmail.com>
2024-08-22 14:21:07 +0900

    :recycle:문혜림: ordalModal.interface.ts 경로변경

--- client/src/interfaces/components/modal/order/OrderModal.interface.ts
+++ client/src/interfaces/components/modal/order/OrderModal.interface.ts
@@ -1,4 +1,4 @@
-import IProduct from "../../../../../db/products/product.interface";
+import IProduct from "../../../../../../db/products/product.interface";
 import { ProductDTO } from "@shared/DTO/products/product.dto";
 
 export default interface OrderModalProps {
