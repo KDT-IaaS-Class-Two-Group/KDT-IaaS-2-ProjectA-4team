cf9c99d99369a481f80b9d805dc9217eac1bc4c7
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-22 14:51:16 +0900

    :recycle:유호영:changePasswordFetch 리터럴값 리팩토링

--- client/src/model/password/change/changePasswordFetch.ts
+++ client/src/model/password/change/changePasswordFetch.ts
@@ -1,5 +1,7 @@
 import fetcher from "src/modules/fetching/fetcher";
 import serverUrlGenerator from "src/modules/generator/serverUrlGenerator";
+import { unknownErrMessage } from "static/error/unknown/unknownErr.static";
+import { changePasswordError } from "static/hooks/password/changePasswordFetch.static";
 
 /**
  * @crystal23733 24.08.01
@@ -29,16 +31,15 @@ export default async (password: string, changePassword: string) => {
 
     if (!response.ok) {
       throw new Error(
-        responseData.message || "비밀번호 변경 중 오류가 발생했습니다.",
+        responseData.message || changePasswordError,
       );
     }
 
     return responseData;
   } catch (error) {
-    console.error("Error in changePasswordFetch:", error);
     if (error instanceof Error) {
       throw error;
     }
-    throw new Error("알 수 없는 오류가 발생했습니다.");
+    throw new Error(unknownErrMessage);
   }
 };
