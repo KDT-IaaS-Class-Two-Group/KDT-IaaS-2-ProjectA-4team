f36b47483be52c3eb72e49524ca0fce341f2587a
jojayeon <jojayeon6152@gmail.com>
2024-08-22 16:59:52 +0900

    :package: 조자연: 리터럴 제거

--- client/src/pages/admin/stockDate.tsx
+++ client/src/pages/admin/stockDate.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import AdminNav from "src/components/nav/admin/adminNav";
 import { ExpirationDataTable } from "src/components/table/expiration/ExpirationDataTable";
-import Logo from "src/components/logo/logo";
 import TitleComponent from "src/components/title/titleComponent";
+import { PAGE_TITLE } from "static/pages/admin/stockDate.syatic";
 /**
  * @jojayeon 24.08.05
  * @returns 유통관리 페이지
@@ -17,7 +17,7 @@ const AdminstockDate: React.FC = () => {
         </div>
         <div className="flex flex-col gap-4 mr-5">
           <div className="items-centerw-auto h-10 py-3">
-            <TitleComponent className="font-jamsil" titletext="유통기한 관리" />
+            <TitleComponent className="font-jamsil" titletext={PAGE_TITLE}/>
           </div>
           <ExpirationDataTable />
         </div>
