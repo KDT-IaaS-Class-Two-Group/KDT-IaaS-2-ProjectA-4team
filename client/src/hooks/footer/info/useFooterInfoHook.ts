2e98212133b255d64e7c87d71629fbd70c2f2f56
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-23 10:09:34 +0900

    :bulb:유호영:useFooterHook JSDoc 수정

--- client/src/hooks/footer/info/useFooterInfoHook.ts
+++ client/src/hooks/footer/info/useFooterInfoHook.ts
@@ -5,13 +5,24 @@ import thrower from "src/modules/throw/thrower";
 import { failFetchedUserInfoMessage } from "static/hooks/footer/info/useFooterInfoHook.static";
 
 /**
+ * `useFooterInfoHook` 훅은 사용자 정보를 가져와서 상태를 관리합니다.
+ *
+ * 이 훅은 사용자 정보를 서버에서 비동기로 가져오고, 사용자 이름을 상태로 관리합니다.
+ * 데이터 로딩 상태와 함께 사용자 이름을 반환하여, 컴포넌트에서 사용자 정보를 비동기적으로 표시할 수 있습니다.
+ *
+ * @hook
  * @crystal23733 24.08.09
- * * `useFooterInfoHook` 훅은 사용자 정보를 가져와서 상태를 관리합니다.
  *
  * @returns {{
  *   userName: string | null,  // 사용자 이름 (정보를 성공적으로 가져온 경우) 또는 null (가져오는 도중 오류가 발생한 경우)
  *   loading: boolean          // 데이터 로딩 상태 (true: 로딩 중, false: 로딩 완료)
  * }}
+ *
+ * @example
+ * const { userName, loading } = useFooterInfoHook();
+ * if (loading) return <div>Loading...</div>;
+ * if (userName) return <div>Welcome, {userName}!</div>;
+ * return <div>No user information available.</div>;
  */
 const useFooterInfoHook = () => {
   const [userName, setUserName] = useState<string | null>(null);
