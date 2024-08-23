d396043ec5aefce40924dd55b45dbd78aa410f7d
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-23 11:29:57 +0900

    :bulb:유호영:sale 스키마 JSDoc 추가

--- server/src/schemas/sale.schema.ts
+++ server/src/schemas/sale.schema.ts
@@ -4,6 +4,12 @@ import { Document, Types } from 'mongoose';
 export type SaleDocument = Sale & Document;
 
 @Schema()
+/**
+ * @typedef ProductSale
+ * @description 판매된 제품에 대한 정보를 나타냅니다.
+ * @property {Types.ObjectId} productID - 제품의 ObjectId를 참조합니다.
+ * @property {number} quantity - 제품의 판매 수량입니다.
+ */
 export class ProductSale {
   @Prop({ type: Types.ObjectId, ref: 'Product', required: true })
   productID: Types.ObjectId;
@@ -15,6 +21,14 @@ export class ProductSale {
 export const ProductSaleSchema = SchemaFactory.createForClass(ProductSale);
 
 @Schema()
+/**
+ * @typedef Sale
+ * @description 판매 기록에 대한 정보를 나타냅니다.
+ * @property {Types.ObjectId} memberID - 판매를 수행한 회원의 ObjectId를 참조합니다.
+ * @property {ProductSale[]} products - 판매된 제품들의 배열입니다.
+ * @property {number} totalPrice - 총 판매 금액입니다.
+ * @property {string} saleDate - 판매 날짜를 나타냅니다.
+ */
 export class Sale {
   @Prop({ type: Types.ObjectId, ref: 'Member', required: true })
   memberID: Types.ObjectId;
