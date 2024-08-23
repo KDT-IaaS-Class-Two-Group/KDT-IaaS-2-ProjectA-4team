7190d4d3d2ec568e303c820ce077f557c14c53df
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-23 10:40:08 +0900

    :bulb:유호영:useSearchHook JSDoc 수정

--- client/src/hooks/useSearchHook.ts
+++ client/src/hooks/useSearchHook.ts
@@ -1,9 +1,17 @@
 import { useState, useCallback } from "react";
 
 /**
- * @function useSearch
- * @description 검색어를 관리하는 훅
- * @returns [searchQuery, setSearchQuery] 검색어 상태와 설정 함수
+ * @crystal23733
+ * @date 24.08.09
+ *
+ * `useSearch` 훅은 검색어를 관리하는 상태 훅입니다.
+ *
+ * @returns {
+ *   searchQuery: string;        // 현재 검색어 상태
+ *   handleSearch: (query: string) => void; // 검색어를 업데이트하는 함수
+ * }
+ *
+ * @function
  */
 const useSearch = () => {
   const [searchQuery, setSearchQuery] = useState("");
