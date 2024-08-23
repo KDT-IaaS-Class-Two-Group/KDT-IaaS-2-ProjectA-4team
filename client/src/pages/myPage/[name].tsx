0b8190585e925cb4875925b72fa48dc23290af90
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-23 10:53:43 +0900

    :bulb:유호영:[name]컴포넌트 JSDoc 추가

--- client/src/pages/myPage/[name].tsx
+++ client/src/pages/myPage/[name].tsx
@@ -7,8 +7,22 @@ import useOrderHook from "src/hooks/order/orderHook";
 import useRedirect from "src/hooks/redirect/useRedirect";
 
 /**
- * @crystal23733 24.08.01
- * @returns {JSXElement}
+ * @crystal23733
+ * @date 24.08.23
+ *
+ * `MyPage` 컴포넌트는 사용자의 마이페이지를 렌더링합니다.
+ *
+ * 이 컴포넌트는 다음을 포함합니다:
+ * - `LoginInfoComponent`: 로그인 사용자 정보를 표시합니다.
+ * - `MyPageFormComponent`: 비밀번호 변경 폼을 렌더링합니다.
+ * - `OrderDetails`: 사용자의 주문 내역을 표시합니다.
+ *
+ * `useOrderHook` 훅을 통해 주문 내역을 가져오고 로딩 상태와 오류를 관리합니다.
+ * `useRedirect` 훅을 통해 페이지 리디렉션 기능을 제공합니다.
+ *
+ * @component
+ *
+ * @returns {JSX.Element} - 사용자의 마이페이지를 구성하는 JSX 요소를 반환합니다.
  */
 const MyPage: React.FC = () => {
   const router = useRouter();
