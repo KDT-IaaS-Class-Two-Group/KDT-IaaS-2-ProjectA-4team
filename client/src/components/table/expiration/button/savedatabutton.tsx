a44c8b776ce072aea507a0b980a700a4c6ffd56a
jojayeon <jojayeon6152@gmail.com>
2024-08-23 11:51:32 +0900

    :bulb: 조자연: savedatabutton JSDoc 추가

--- client/src/components/table/expiration/button/savedatabutton.tsx
+++ client/src/components/table/expiration/button/savedatabutton.tsx
@@ -4,9 +4,15 @@ import ButtonComponent from "src/components/button/customized/CustomButton";
 import { useExpirationDate } from "src/model/expiration/useExpirationDate";
 import { ProductDTO } from "@shared/DTO/products/product.dto";
 import { PRODUCT_ORDER } from "static/components/table/expiration/button/savedatabutton.static";
+
 /**
  * @jojayeon 24.08.09
- * @returns 제품 구매 버튼
+ * @returns 제품 구매 버튼 ,새로운 제품으 구매하는 버튼 역할을 합니다. 
+ * @returns {React.ReactElement}  제품 구매 버튼과 제품 추가 모달을 포함하는 React 컴포넌트입니다.
+ * @compoent 
+ * * openModal - 모달창 열기
+ * * closeModal - 모달창 닫기
+ * * handleAddProduct - 주문하는 행동 (주문 처리하고 모달창 닫기)
  */
 const ProductPage: React.FC = () => {
   const { addProduct } = useExpirationDate();
@@ -28,6 +34,7 @@ const ProductPage: React.FC = () => {
     }
   };
 
+  //버튼이 보는 코드 
   return (
     <div className="w-24 h-8 mt-6 text-center bg-cyan-500 rounded-xl text-white text-sm leading-loose border border-transparent hover:text-cyan-500 hover:bg-white hover:border hover:border-cyan-500">
       <ButtonComponent onClick={openModal} variant="default">
