c9542cb9ef9e40c7a4c1610381883079fe936ac2
jojayeon <jojayeon6152@gmail.com>
2024-08-23 11:52:38 +0900

    :bulb: 조자연: useAddProduct JSDoc 추가

--- client/src/model/expiration/useAddProduct/useAddProduct.ts
+++ client/src/model/expiration/useAddProduct/useAddProduct.ts
@@ -3,10 +3,13 @@ import serverUrlGenerator from "src/modules/generator/serverUrlGenerator";
 import { ProductDTO } from "@shared/DTO/products/product.dto";
 
 /**
+ * @jojayeon 24.08.22
  * 제품 추가를 처리하는 훅
- * @param {Function} fetchData - 데이터를 다시 가져오는 함수
+ * @component
+ * @param {Function} fetchData - 데이터를 post로 postUrl주소로 보내는 작업
  * @param {string} epProductsDate - 제품 날짜를 위한 엔드포인트
- * @returns {Function} 제품 추가 함수
+ * @returns {Function} addProduct - ProductDTO타입의 product데이터를 비동기로 서버에 보내는역할
+ *
  */
 export const useAddProduct = (fetchData: () => void, epProductsDate: string) => {
   const addProduct = async (product: ProductDTO) => {
